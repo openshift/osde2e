5160dc873db1c7d09564c3038aa1552924a36b02
Dan Gillespie <dgillesp@redhat.com>
2019-05-21 04:08:39 -0700

    change(*): allow TEST_KUBECONFIG to be specified without setting up UHC

--- setup.go
+++ setup.go
@@ -62,6 +62,11 @@ var _ = ginkgo.AfterSuite(func() {
 
 // setupCluster brings up a cluster, waits for it to be ready, then returns it's name.
 func setupCluster(cfg *config.Config) (err error) {
+	// if TEST_KUBECONFIG has been set, skip configuring UHC
+	if len(cfg.Kubeconfig) > 0 {
+		return useKubeconfig(cfg)
+	}
+
 	if OSD, err = osd.New(cfg.UHCToken, !cfg.UseProd, cfg.DebugOSD); err != nil {
 		return fmt.Errorf("could not setup OSD: %v", err)
 	}
@@ -92,18 +97,20 @@ func setupCluster(cfg *config.Config) (err error) {
 		return fmt.Errorf("failed waiting for cluster ready: %v", err)
 	}
 
-	if len(cfg.Kubeconfig) == 0 {
-		if cfg.Kubeconfig, err = OSD.ClusterKubeconfig(cfg.ClusterID); err != nil {
-			return fmt.Errorf("could not get kubeconfig for cluster: %v", err)
-		}
-	} else {
-		filename := string(cfg.Kubeconfig)
-		cfg.Kubeconfig, err = ioutil.ReadFile(filename)
-		if err != nil {
-			return fmt.Errorf("failed reading '%s' which has been set as the TEST_KUBECONFIG: %v", filename, err)
-		}
-		log.Printf("Using a set TEST_KUBECONFIG of '%s' for Origin API calls.", filename)
+	if cfg.Kubeconfig, err = OSD.ClusterKubeconfig(cfg.ClusterID); err != nil {
+		return fmt.Errorf("could not get kubeconfig for cluster: %v", err)
 	}
+	return nil
+}
+
+// useKubeconfig reads the path provided for a TEST_KUBECONFIG and uses it for testing.
+func useKubeconfig(cfg *config.Config) (err error) {
+	filename := string(cfg.Kubeconfig)
+	cfg.Kubeconfig, err = ioutil.ReadFile(filename)
+	if err != nil {
+		return fmt.Errorf("failed reading '%s' which has been set as the TEST_KUBECONFIG: %v", filename, err)
+	}
+	log.Printf("Using a set TEST_KUBECONFIG of '%s' for Origin API calls.", filename)
 	return nil
 }
 
