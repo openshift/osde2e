1755434b406e4ae45fc24b71b238b3886767d54e
Dan Gillespie <dgillesp@redhat.com>
2019-05-28 14:22:42 -0700

    change(test/verify): print Pods that aren't ready

--- test/verify/pods.go
+++ test/verify/pods.go
@@ -22,17 +22,19 @@ var _ = ginkgo.Describe("Pods", func() {
 		Expect(err).NotTo(HaveOccurred(), "couldn't list Pods")
 		Expect(list).NotTo(BeNil())
 
-		var running float64
+		var notReady []v1.Pod
 		for _, pod := range list.Items {
 			phase := pod.Status.Phase
-			if phase == v1.PodRunning || phase == v1.PodSucceeded {
-				running++
+			if phase != v1.PodRunning && phase != v1.PodSucceeded {
+				notReady = append(notReady, pod)
 			}
 		}
 
-		ratio := (running / float64(len(list.Items))) * 100
+		total := float64(len(list.Items))
+		ready := total - float64(len(notReady))
+		ratio := (ready / total) * 100
 		Expect(ratio).Should(Equal(requiredRatio),
-			"only %f%% of clusters ready, need %f%%", ratio, requiredRatio)
+			"only %f%% of Pods ready, need %f%%. Not ready: %s", ratio, requiredRatio, listPodPhases(notReady))
 	})
 
 	ginkgo.It("should not be Failed", func() {
@@ -44,3 +46,13 @@ var _ = ginkgo.Describe("Pods", func() {
 		Expect(list.Items).Should(HaveLen(0), "'%d' Pods are 'Failed'", len(list.Items))
 	})
 })
+
+func listPodPhases(pods []v1.Pod) (out string) {
+	for i, pod := range pods {
+		if i != 0 {
+			out += ", "
+		}
+		out += fmt.Sprintf("%s/%s (Phase: %s)", pod.Namespace, pod.Name, pod.Status.Phase)
+	}
+	return
+}
