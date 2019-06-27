af523d9da3d89aa6ab53ae23ccc5f4f2c5c1f6e3
Dan Gillespie <dgillesp@redhat.com>
2019-06-27 11:52:30 -0700

    change(pkg/config): split into 2 files

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,17 +1,8 @@
 // Package config provides the configuration for tests run as part of the osde2e suite.
 package config
 
-import (
-	"os"
-	"reflect"
-)
-
 // Cfg is the configuration used for end to end testing.
 var Cfg = new(Config)
-
-func init() {
-	Cfg.LoadFromEnv()
-}
 
 // Config dictates the behavior of cluster tests.
 type Config struct {
@@ -62,25 +53,4 @@ type Config struct {
 
 	// DebugOSD shows debug level messages when enabled.
 	DebugOSD bool `env:"DEBUG_OSD"`
-}
-
-// LoadFromEnv sets values from environment variables specified in `env` tags.
-func (c *Config) LoadFromEnv() {
-	v := reflect.ValueOf(c).Elem()
-	for i := 0; i < v.Type().NumField(); i++ {
-		f := v.Type().Field(i)
-		if env, ok := f.Tag.Lookup("env"); ok {
-			if envVal := os.Getenv(env); len(envVal) > 0 {
-				field := v.Field(i)
-				switch f.Type.Kind() {
-				case reflect.String:
-					field.SetString(envVal)
-				case reflect.Bool:
-					field.SetBool(true)
-				case reflect.Slice:
-					field.SetBytes([]byte(envVal))
-				}
-			}
-		}
-	}
 }
---
+++ pkg/config/load.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"os"
+	"reflect"
+)
+
+func init() {
+	Cfg.LoadFromEnv()
+}
+
+// LoadFromEnv sets values from environment variables specified in `env` tags.
+func (c *Config) LoadFromEnv() {
+	v := reflect.ValueOf(c).Elem()
+	for i := 0; i < v.Type().NumField(); i++ {
+		f := v.Type().Field(i)
+		if env, ok := f.Tag.Lookup("env"); ok {
+			if envVal := os.Getenv(env); len(envVal) > 0 {
+				field := v.Field(i)
+				switch f.Type.Kind() {
+				case reflect.String:
+					field.SetString(envVal)
+				case reflect.Bool:
+					field.SetBool(true)
+				case reflect.Slice:
+					field.SetBytes([]byte(envVal))
+				}
+			}
+		}
+	}
+}
