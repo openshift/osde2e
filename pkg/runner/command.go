e2c83200cd9bc3d3d1f72cc0186c9613b7474645
Dan Gillespie <dgillesp@redhat.com>
2019-07-02 08:27:05 -0700

    fix(pkg/runner): include name in stdout & stderr

--- pkg/runner/command.go
+++ pkg/runner/command.go
@@ -18,7 +18,7 @@ oc config use-context {{.Name}}
 mkdir -p {{.OutputDir}}
 
 # run Cmd and preserve it's stdout and stderr
-{{.Cmd}} > >(tee {{.OutputDir}}/runner-out.txt) 2> >(tee {{.OutputDir}}/runner-err.txt >&2)
+{{.Cmd}} > >(tee -a {{.OutputDir}}/{{.Name}}-out.txt) 2> >(tee -a {{.OutputDir}}/{{.Name}}-err.txt >&2)
 
 # create a Tarball of OutputDir if requested
 {{$outDir := .OutputDir}}
