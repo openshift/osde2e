ce2e6ef11501eab12a2f959ef1c519a8acfa0492
Dan Gillespie <dgillesp@redhat.com>
2019-08-07 09:08:45 -0700

    add(pkg/config): docs sections

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -19,40 +19,40 @@ var Cfg = new(Config)
 // Config dictates the behavior of cluster tests.
 type Config struct {
 	// ReportDir is the location JUnit XML results are written.
-	ReportDir string `env:"REPORT_DIR"`
+	ReportDir string `env:"REPORT_DIR" sect:"tests"`
 
 	// Suffix is used at the end of test names to identify them.
-	Suffix string
+	Suffix string `env:"SUFFIX" sect:"tests"`
 
 	// UHCToken is used to authenticate with UHC.
-	UHCToken string `env:"UHC_TOKEN"`
+	UHCToken string `env:"UHC_TOKEN" sect:"required"`
 
 	// ClusterID identifies the cluster. If set at start, an existing cluster is tested.
-	ClusterID string `env:"CLUSTER_ID"`
+	ClusterID string `env:"CLUSTER_ID" sect:"cluster"`
 
 	// ClusterName is the name of the cluster being created.
-	ClusterName string `env:"CLUSTER_NAME"`
+	ClusterName string `env:"CLUSTER_NAME" sect:"cluster"`
 
 	// ClusterVersion is the version of the cluster being deployed.
-	ClusterVersion string `env:"CLUSTER_VERSION"`
+	ClusterVersion string `env:"CLUSTER_VERSION" sect:"version"`
 
 	// MajorTarget is the major version to target. If specified, it is used in version selection.
-	MajorTarget int64 `env:"MAJOR_TARGET"`
+	MajorTarget int64 `env:"MAJOR_TARGET" sect:"version"`
 
 	// MinorTarget is the minor version to target. If specified, it is used in version selection.
-	MinorTarget int64 `env:"MINOR_TARGET"`
+	MinorTarget int64 `env:"MINOR_TARGET" sect:"version"`
 
 	// ClusterUpTimeout is how long to wait before failing a cluster launch.
 	ClusterUpTimeout time.Duration
 
 	// TestGridBucket is the Google Cloud Storage bucket where results are reported for TestGrid.
-	TestGridBucket string `env:"TESTGRID_BUCKET"`
+	TestGridBucket string `env:"TESTGRID_BUCKET" sect:"testgrid"`
 
 	// TestGridPrefix is used to namespace reports.
-	TestGridPrefix string `env:"TESTGRID_PREFIX"`
+	TestGridPrefix string `env:"TESTGRID_PREFIX" sect:"testgrid"`
 
 	// TestGridServiceAccount is a Base64 encoded Google Cloud Service Account used to access the TestGridBucket.
-	TestGridServiceAccount []byte `env:"TESTGRID_SERVICE_ACCOUNT"`
+	TestGridServiceAccount []byte `env:"TESTGRID_SERVICE_ACCOUNT" sect:"testgrid"`
 
 	// UseProd sends requests to production OSD.
 	//
@@ -60,32 +60,32 @@ type Config struct {
 	UseProd bool `env:"USE_PROD"`
 
 	// MultiAZ deploys a cluster across multiple availability zones.
-	MultiAZ bool `env:"MULTI_AZ"`
+	MultiAZ bool `env:"MULTI_AZ" sect:"cluster"`
 
 	// NoDestroy leaves the cluster running after testing.
-	NoDestroy bool `env:"NO_DESTROY"`
+	NoDestroy bool `env:"NO_DESTROY" sect:"cluster"`
 
 	// NoTestGrid disables reporting to TestGrid.
-	NoTestGrid bool `env:"NO_TESTGRID"`
+	NoTestGrid bool `env:"NO_TESTGRID" sect:"testgrid"`
 
 	// Kubeconfig is used to access a cluster.
-	Kubeconfig []byte `env:"TEST_KUBECONFIG"`
+	Kubeconfig []byte `env:"TEST_KUBECONFIG" sect:"cluster"`
 
 	// OSDEnv is the OpenShift Dedicated environment used to provision clusters.
-	OSDEnv string `env:"OSD_ENV"`
+	OSDEnv string `env:"OSD_ENV" sect:"environment"`
 
 	// DebugOSD shows debug level messages when enabled.
-	DebugOSD bool `env:"DEBUG_OSD"`
+	DebugOSD bool `env:"DEBUG_OSD" sect:"environment"`
 
 	// CleanRuns is the number of times the test-version is run before skipping.
-	CleanRuns int `env:"CLEAN_RUNS"`
+	CleanRuns int `env:"CLEAN_RUNS" sect:"tests"`
 
 	// UpgradeReleaseStream used to retrieve latest release images. If set, it will be used to perform an upgrade.
-	UpgradeReleaseStream string `env:"UPGRADE_RELEASE_STREAM"`
+	UpgradeReleaseStream string `env:"UPGRADE_RELEASE_STREAM" sect:"upgrade"`
 
 	// UpgradeReleaseName is the name of the release in a release stream. UpgradeReleaseStream must be set.
-	UpgradeReleaseName string `env:"UPGRADE_RELEASE_NAME"`
+	UpgradeReleaseName string `env:"UPGRADE_RELEASE_NAME" sect:"upgrade"`
 
 	// UpgradeImage is the release image a cluster is upgraded to. If set, it overrides the release stream and upgrades.
-	UpgradeImage string `env:"UPGRADE_IMAGE"`
+	UpgradeImage string `env:"UPGRADE_IMAGE" sect:"upgrade"`
 }
