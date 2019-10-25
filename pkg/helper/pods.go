71b49e2c9efc5ff52a3be73ab409fe355d8b327e
Michael Wilson <miwilson@redhat.com>
2019-10-25 00:20:34 -0400

    Revert "Merge pull request #135 from jeefy/cvo-check"
    This reverts commit 7edd32f85722bbd253872443e31cc3d00b7ed454, reversing
    changes made to 4f154d1b4a69a74ca4c6bec7283938b8a5fc9e4d.

--- pkg/helper/pods.go
+++ pkg/helper/pods.go
@@ -8,7 +8,6 @@ import (
 
 	kubev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
 )
 
 // WaitForPodPhase until in target, checking n times and sleeping dur between them. Last known phase is returned.
@@ -32,32 +31,4 @@ func (h *H) WaitForPodPhase(pod *kubev1.Pod, target kubev1.PodPhase, n int, dur 
 
 	Expect(phase).NotTo(BeEmpty())
 	return
-}
-
-// CheckPodHealth attempts to look at the state of all pods and returns true if things are healthy.
-func CheckPodHealth(kubeclient kubernetes.Interface) (bool, error) {
-	var notReady []kubev1.Pod
-
-	log.Print("Checking that all Pods are running or completed...")
-
-	list, err := kubeclient.CoreV1().Pods(metav1.NamespaceAll).List(metav1.ListOptions{})
-	if err != nil {
-		return false, err
-	}
-	Expect(list).NotTo(BeNil())
-
-	for _, pod := range list.Items {
-		phase := pod.Status.Phase
-		if phase != kubev1.PodRunning && phase != kubev1.PodSucceeded {
-			notReady = append(notReady, pod)
-		}
-	}
-
-	total := len(list.Items)
-	ready := float64(total - len(notReady))
-	curRatio := (ready / float64(total)) * 100
-
-	log.Printf("%v%% of pods are currently alive: ", curRatio)
-
-	return len(notReady) == 0, nil
 }
