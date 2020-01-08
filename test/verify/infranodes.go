5eff4ef155f7dcf20b01206de38ab93c38b20a24
Manuel Dewald <mdewald@redhat.com>
2020-01-08 13:41:36 +0100

    check managed-velero-operator and velero are deployed to infra node

--- test/verify/infranodes.go
+++ test/verify/infranodes.go
@@ -1,8 +1,6 @@
 package verify
 
 import (
-	"fmt"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
@@ -35,18 +33,21 @@ var _ = Describe("Infranodes", func() {
 	})
 
 	DescribeTable("contain pods scheduled to run there",
-		func(podName, podNamespace string) {
+		func(labelSelector, podNamespace string) {
 			podListOptions := metav1.ListOptions{}
-			podListOptions.LabelSelector = fmt.Sprintf("name=%s", podName)
+			podListOptions.LabelSelector = labelSelector
 			pods, err := h.Kube().CoreV1().Pods(podNamespace).List(podListOptions)
 			Expect(err).NotTo(HaveOccurred(), "Failed to list pods in %s namespace", podNamespace)
+			Expect(len(pods.Items)).To(BeNumerically(">", 0), "no pod with label %s found", labelSelector)
 			for _, pod := range pods.Items {
 				Expect(infraNodeAddresses).To(ContainElement(pod.Status.HostIP), "pod %s/%s not scheduled to infra node", pod.Namespace, pod.Name)
 			}
 		},
-		Entry("sre-machine-api-status-exporter pod", "sre-machine-api-status-exporter", "openshift-monitoring"),
-		Entry("sre-ebs-iops-reporter pod", "sre-ebs-iops-reporter", "openshift-monitoring"),
-		Entry("sre-stuck-ebs-vols pod", "sre-stuck-ebs-vols", "openshift-monitoring"),
+		Entry("sre-machine-api-status-exporter pod", "name=sre-machine-api-status-exporter", "openshift-monitoring"),
+		Entry("sre-ebs-iops-reporter pod", "name=sre-ebs-iops-reporter", "openshift-monitoring"),
+		Entry("sre-stuck-ebs-vols pod", "name=sre-stuck-ebs-vols", "openshift-monitoring"),
+		Entry("managed-velero-operator pod", "name=managed-velero-operator", "openshift-velero"),
+		Entry("velero pod", "component=velero", "openshift-velero"),
 	)
 })
 
