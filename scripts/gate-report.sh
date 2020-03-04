e2e0f62f323f4412e262962b84f61cb59143cb62
Michael Wilson <miwilson@redhat.com>
2020-03-04 14:52:07 -0500

    SDCICD-173. Gate report script.
    A gate report script has been added that will wrap gate report
    production and S3 uploading into an executable script. This will make
    job creation of the gate report easier.

---
+++ scripts/gate-report.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# gate-report will run the gate report command against the provided arguments and upload the resulting report to the osde2e-metrics bucket.
+#
+
+set -e
+
+SRC_DIR="$(cd $(dirname $0)/..; pwd)"
+METRICS_BUCKET=osde2e-metrics
+GATE_REPORT=gate-report
+VENV="$(mktemp -d)"
+REPORT_DIR="$(mktemp -d)"
+
+trap 'rm -rf "$VENV" "$REPORT_DIR"' EXIT
+
+if [[ $# -ne 2 ]]; then
+	echo "Usage: $0 <environment> <version>"
+	exit 1
+fi
+
+ENVIRONMENT="$1"
+VERSION="$2"
+
+make build -f "$SRC_DIR/Makefile"
+
+virtualenv "$VENV"
+. "$VENV/bin/activate"
+
+pip install awscli
+
+if ! aws s3 ls s3://$METRICS_BUCKET 2>&1 > /dev/null ; then
+	echo "AWS CLI not configured properly."
+	exit 1
+fi
+
+REPORT_FILE="$REPORT_DIR/$VERSION-report.json"
+
+osde2e gate -output "$REPORT_FILE" "$ENVIRONMENT" "$VERSION"
+
+aws s3 cp "$REPORT_FILE" "s3://$METRICS_BUCKET/$GATE_REPORT/$REPORT_FILE"
