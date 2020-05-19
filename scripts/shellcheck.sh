96eaf8427f0e22f2d36043692f08279be0bf3bf4
Panagiotis Georgiadis <pgeorgia@redhat.com>
2020-05-19 13:49:11 +0200

    Optimize shellcheck tarball fetch
    Fetch the correct tarball based on the operating system and
    architecture.

--- scripts/shellcheck.sh
+++ scripts/shellcheck.sh
@@ -5,5 +5,7 @@
 
 SCRIPT_DIR="$(dirname "$0")"
 scversion="stable" # or "v0.4.7", or "latest"
-[ ! -d "$SCRIPT_DIR/shellcheck-${scversion?}" ] && wget -qO- "https://github.com/koalaman/shellcheck/releases/download/${scversion?}/shellcheck-${scversion?}.linux.x86_64.tar.xz" | tar -xJv -C "$SCRIPT_DIR"
+os=$(uname -s)
+arch=$(uname -m)
+[ ! -d "$SCRIPT_DIR/shellcheck-${scversion?}" ] && wget -qO- "https://github.com/koalaman/shellcheck/releases/download/${scversion?}/shellcheck-${scversion?}.${os}.${arch}.tar.xz" | tar -xJv -C "$SCRIPT_DIR"
 "$SCRIPT_DIR/shellcheck-${scversion?}/shellcheck" "$@"
