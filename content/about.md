68c7c3aa973533832376561e2c631d7d4ebd1044
Michael Wilson <miwilson@redhat.com>
2020-08-03 20:57:42 -0400

    More tweaks to display, script for generating weather reports.
    There are various tweaks to displaying reports as well as a script for
    generating weather reports.

---
+++ content/about.md
@@ -0,0 +1,7 @@
+---
+title: 'About this page'
+button: 'About us'
+weight: 2
+---
+
+Automatically generated reports generated from OSDe2e data live here.
---
+++ scripts/generate-reports.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+DIR=$(cd "$(dirname "$0")"/..; pwd)
+#OSDE2E=quay.io/app-sre/osde2e
+OSDE2E=8d2d6c0370c5
+
+run_osde2e() {
+	WEATHER_PROVIDER=$1 JOB_ALLOWLIST="osde2e-.*-$1-e2e-.*" docker run -u "$(id -u)" -v "$DIR:/hugo-site" -e WEATHER_PROVIDER -e JOB_ALLOWLIST -e PROMETHEUS_ADDRESS -e PROMETHEUS_BEARER_TOKEN "$OSDE2E" weather-report --output "/hugo-site/content/post/$(uuidgen | sed s/-//g).md" --outputType sd-report
+}
+
+run_osde2e aws
+run_osde2e gcp
+run_osde2e moa
+
+docker run -u $(id -u) -v "$DIR:/hugo-site" klakegg/hugo:0.74.3-ext -s /hugo-site --cleanDestinationDir
