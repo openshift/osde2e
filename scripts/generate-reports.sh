50fd778b160049dc200512010b2eea173491a606
Michael Wilson <miwilson@redhat.com>
2020-08-04 01:38:44 -0400

    Update script to use actual quay.io osde2e image.

--- scripts/generate-reports.sh
+++ scripts/generate-reports.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 DIR=$(cd "$(dirname "$0")"/..; pwd)
-#OSDE2E=quay.io/app-sre/osde2e
-OSDE2E=8d2d6c0370c5
+OSDE2E=quay.io/app-sre/osde2e
 
 run_osde2e() {
 	WEATHER_PROVIDER=$1 JOB_ALLOWLIST="osde2e-.*-$1-e2e-.*" docker run -u "$(id -u)" -v "$DIR:/hugo-site" -e WEATHER_PROVIDER -e JOB_ALLOWLIST -e PROMETHEUS_ADDRESS -e PROMETHEUS_BEARER_TOKEN "$OSDE2E" weather-report --output "/hugo-site/content/post/$(uuidgen | sed s/-//g).md" --outputType sd-report
