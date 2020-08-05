cc089f7b1cb6d6ae2cf6309b08814b6beba2efd6
Jeffrey Sica <jeef111x@gmail.com>
2020-08-05 12:17:00 -0400

    change default name from ci-cluster to osde2e

--- pkg/common/config/log_metrics_test.go
+++ pkg/common/config/log_metrics_test.go
@@ -13,27 +13,27 @@ func TestHasMatches(t *testing.T) {
 		{
 			Name:  "match found",
 			Regex: "no such host",
-			Text: `time="2020-05-09T16:12:33Z" level=debug msg="Still waiting for the Kubernetes API: Get https://api.ci-cluster-4-3.b9t7.p2.openshiftapps.com:6443/version?timeout=32s: dial tcp: lookup api.ci-cluster-4-3.b9t7.p2.openshiftapps.com on 10.121.6.76:53: no such host"
-time="2020-05-09T16:12:33Z" level=debug msg="Still waiting for the Kubernetes API: Get https://api.ci-cluster-4-3.b9t7.p2.openshiftapps.com:6443/version?timeout=32s: dial tcp: lookup api.ci-cluster-4-3.b9t7.p2.openshiftapps.com on 10.121.6.76:53: no such host"
-time="2020-05-09T16:12:33Z" level=debug msg="Still waiting for the Kubernetes API: Get https://api.ci-cluster-4-3.b9t7.p2.openshiftapps.com:6443/version?timeout=32s: dial tcp: lookup api.ci-cluster-4-3.b9t7.p2.openshiftapps.com on 10.121.6.76:53: no such host"`,
+			Text: `time="2020-05-09T16:12:33Z" level=debug msg="Still waiting for the Kubernetes API: Get https://api.osde2e-4-3.b9t7.p2.openshiftapps.com:6443/version?timeout=32s: dial tcp: lookup api.osde2e-4-3.b9t7.p2.openshiftapps.com on 10.121.6.76:53: no such host"
+time="2020-05-09T16:12:33Z" level=debug msg="Still waiting for the Kubernetes API: Get https://api.osde2e-4-3.b9t7.p2.openshiftapps.com:6443/version?timeout=32s: dial tcp: lookup api.osde2e-4-3.b9t7.p2.openshiftapps.com on 10.121.6.76:53: no such host"
+time="2020-05-09T16:12:33Z" level=debug msg="Still waiting for the Kubernetes API: Get https://api.osde2e-4-3.b9t7.p2.openshiftapps.com:6443/version?timeout=32s: dial tcp: lookup api.osde2e-4-3.b9t7.p2.openshiftapps.com on 10.121.6.76:53: no such host"`,
 			IgnoreStrings:           []string{},
 			ExpectedNumberOfMatches: 3,
 		},
 		{
 			Name:  "no match found",
 			Regex: "blah blah blah",
-			Text: `time="2020-05-09T16:12:33Z" level=debug msg="Still waiting for the Kubernetes API: Get https://api.ci-cluster-4-3.b9t7.p2.openshiftapps.com:6443/version?timeout=32s: dial tcp: lookup api.ci-cluster-4-3.b9t7.p2.openshiftapps.com on 10.121.6.76:53: no such host"
-time="2020-05-09T16:12:33Z" level=debug msg="Still waiting for the Kubernetes API: Get https://api.ci-cluster-4-3.b9t7.p2.openshiftapps.com:6443/version?timeout=32s: dial tcp: lookup api.ci-cluster-4-3.b9t7.p2.openshiftapps.com on 10.121.6.76:53: no such host"
-time="2020-05-09T16:12:33Z" level=debug msg="Still waiting for the Kubernetes API: Get https://api.ci-cluster-4-3.b9t7.p2.openshiftapps.com:6443/version?timeout=32s: dial tcp: lookup api.ci-cluster-4-3.b9t7.p2.openshiftapps.com on 10.121.6.76:53: no such host"`,
+			Text: `time="2020-05-09T16:12:33Z" level=debug msg="Still waiting for the Kubernetes API: Get https://api.osde2e-4-3.b9t7.p2.openshiftapps.com:6443/version?timeout=32s: dial tcp: lookup api.osde2e-4-3.b9t7.p2.openshiftapps.com on 10.121.6.76:53: no such host"
+time="2020-05-09T16:12:33Z" level=debug msg="Still waiting for the Kubernetes API: Get https://api.osde2e-4-3.b9t7.p2.openshiftapps.com:6443/version?timeout=32s: dial tcp: lookup api.osde2e-4-3.b9t7.p2.openshiftapps.com on 10.121.6.76:53: no such host"
+time="2020-05-09T16:12:33Z" level=debug msg="Still waiting for the Kubernetes API: Get https://api.osde2e-4-3.b9t7.p2.openshiftapps.com:6443/version?timeout=32s: dial tcp: lookup api.osde2e-4-3.b9t7.p2.openshiftapps.com on 10.121.6.76:53: no such host"`,
 			IgnoreStrings:           []string{},
 			ExpectedNumberOfMatches: 0,
 		},
 		{
 			Name:  "match found but ignored",
 			Regex: "no such host",
-			Text: `time="2020-05-09T16:12:33Z" level=debug msg="Still waiting for the Kubernetes API: Get https://api.ci-cluster-4-3.b9t7.p2.openshiftapps.com:6443/version?timeout=32s: dial tcp: lookup api.ci-cluster-4-3.b9t7.p2.openshiftapps.com on 10.121.6.76:53: no such host"
-time="2020-05-09T16:12:33Z" level=debug msg="Still waiting for the Kubernetes API: Get https://api.ci-cluster-4-3.b9t7.p2.openshiftapps.com:6443/version?timeout=32s: dial tcp: lookup api.ci-cluster-4-3.b9t7.p2.openshiftapps.com on 10.121.6.76:53: no such host"
-time="2020-05-09T16:12:33Z" level=debug msg="Still waiting for the Kubernetes API: Get https://api.ci-cluster-4-3.b9t7.p2.openshiftapps.com:6443/version?timeout=32s: dial tcp: lookup api.ci-cluster-4-3.b9t7.p2.openshiftapps.com on 10.121.6.76:53: no such host"`,
+			Text: `time="2020-05-09T16:12:33Z" level=debug msg="Still waiting for the Kubernetes API: Get https://api.osde2e-4-3.b9t7.p2.openshiftapps.com:6443/version?timeout=32s: dial tcp: lookup api.osde2e-4-3.b9t7.p2.openshiftapps.com on 10.121.6.76:53: no such host"
+time="2020-05-09T16:12:33Z" level=debug msg="Still waiting for the Kubernetes API: Get https://api.osde2e-4-3.b9t7.p2.openshiftapps.com:6443/version?timeout=32s: dial tcp: lookup api.osde2e-4-3.b9t7.p2.openshiftapps.com on 10.121.6.76:53: no such host"
+time="2020-05-09T16:12:33Z" level=debug msg="Still waiting for the Kubernetes API: Get https://api.osde2e-4-3.b9t7.p2.openshiftapps.com:6443/version?timeout=32s: dial tcp: lookup api.osde2e-4-3.b9t7.p2.openshiftapps.com on 10.121.6.76:53: no such host"`,
 			IgnoreStrings:           []string{"Still waiting for the Kubernetes API"},
 			ExpectedNumberOfMatches: 0,
 		},
