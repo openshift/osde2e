6163335f71a59c6fcc95229017f6b268dc2a4561
Michael Wilson <miwilson@redhat.com>
2020-08-11 11:10:52 -0400

    SDCICD-416. Use updated syntax for reporting.
    The reporting command has changed, so use updated reporting syntax.

--- scripts/generate-weather-reports.sh
+++ scripts/generate-weather-reports.sh
@@ -5,7 +5,7 @@ OSDE2E=quay.io/app-sre/osde2e
 HUGO=quay.io/jlelse/hugo:0.74.3
 
 run_osde2e() {
-	WEATHER_PROVIDER=$1 JOB_ALLOWLIST="osde2e-.*-$1-e2e-.*" docker run -u "$(id -u)" -v "$DIR:/hugo-site" -e WEATHER_PROVIDER -e JOB_ALLOWLIST -e PROMETHEUS_ADDRESS -e PROMETHEUS_BEARER_TOKEN "$OSDE2E" weather-report --output "/hugo-site/content/post/$(uuidgen | sed s/-//g).md" --outputType sd-report
+	WEATHER_PROVIDER=$1 JOB_ALLOWLIST="osde2e-.*-$1-e2e-.*" docker run -u "$(id -u)" -v "$DIR:/hugo-site" -e WEATHER_PROVIDER -e JOB_ALLOWLIST -e PROMETHEUS_ADDRESS -e PROMETHEUS_BEARER_TOKEN "$OSDE2E" report weather-report sd-report --output "/hugo-site/content/post/$(uuidgen | sed s/-//g).md"
 }
 
 docker pull $OSDE2E
