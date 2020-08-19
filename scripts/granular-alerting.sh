0c31826473ae85e1f623d38973b216ff59062e48
Jeffrey Sica <jeef111x@gmail.com>
2020-08-18 20:44:14 -0400

    update slack token env var

--- scripts/granular-alerting.sh
+++ scripts/granular-alerting.sh
@@ -6,4 +6,4 @@
 set -e
 
 docker pull quay.io/app-sre/osde2e
-docker run -e PROMETHEUS_ADDRESS -e PROMETHEUS_BEARER_TOKEN -e SLACK_WEBHOOK quay.io/app-sre/osde2e alert
+docker run -e PROMETHEUS_ADDRESS -e PROMETHEUS_BEARER_TOKEN -e SLACK_API_TOKEN quay.io/app-sre/osde2e alert
