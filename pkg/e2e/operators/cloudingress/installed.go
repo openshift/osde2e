fbe39c6fd079b31b84a8c5599e739db56827b62b
Alice Hubenko <ahubenko@redhat.com>
2020-10-12 14:39:34 -0700

    Added test to verify cloud-ingress-operator is installed.

--- pkg/e2e/operators/cloudingress/installed.go
+++ pkg/e2e/operators/cloudingress/installed.go
@@ -2,32 +2,94 @@ package cloudingress
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"strings"
+	"time"
 
 	"github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
 	"github.com/openshift/osde2e/pkg/common/helper"
+	"github.com/spf13/viper"
 
+	"github.com/openshift/osde2e/pkg/common/config"
+	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // tests
+
 var _ = ginkgo.Describe(CloudIngressTestName, func() {
+
+	var defaultDesiredReplicas int32 = 1
+
 	h := helper.New()
 
-	ginkgo.It("is a placeholder", func() {
-		_, _ = h.Kube().CoreV1().Pods(CloudIngressNamespace).Get(context.TODO(), "something", metav1.GetOptions{})
+	// Check that the operator deployment exists in the operator namespace
+	ginkgo.Context("deployment", func() {
+		ginkgo.It("should exist", func() {
+			deployment, err := pollDeployment(h, CloudIngressNamespace, OperatorName)
+			Expect(err).ToNot(HaveOccurred(), "failed fetching deployment")
+			Expect(deployment).NotTo(BeNil(), "deployment is nil")
+		}, float64(viper.GetFloat64(config.Tests.PollingTimeout)))
 
-		tests := []struct {
-			Name string
-		}{}
+		ginkgo.It("should have all desired replicas ready", func() {
+			deployment, err := pollDeployment(h, CloudIngressNamespace, OperatorName)
+			Expect(err).ToNot(HaveOccurred(), "failed fetching deployment")
 
-		for _, test := range tests {
-			Expect(test.Name).To(Equal(""))
-		}
+			readyReplicas := deployment.Status.ReadyReplicas
+			desiredReplicas := deployment.Status.Replicas
+
+			// The desired replicas should match the default installed replica count
+			Expect(desiredReplicas).To(BeNumerically("==", defaultDesiredReplicas), "The deployment desired replicas should not drift from the default 1.")
+
+			// Desired replica count should match ready replica count
+			Expect(readyReplicas).To(BeNumerically("==", desiredReplicas), "All desired replicas should be ready.")
+		}, float64(viper.GetFloat64(config.Tests.PollingTimeout)))
 	})
+
 })
 
-// utils
+func pollDeployment(h *helper.H, namespace, deploymentName string) (*appsv1.Deployment, error) {
+	// pollDeployment polls for a deployment with a timeout
+	// to handle the case when a new cluster is up but the OLM has not yet
+	// finished deploying the operator
 
-// common setup and utils are in cloudingress.go
+	var err error
+	var deployment *appsv1.Deployment
+
+	// interval is the duration in seconds between polls
+	// values here for humans
+	interval := 5
+
+	// convert time.Duration type
+	timeoutDuration := time.Duration(viper.GetFloat64(config.Tests.PollingTimeout)) * time.Minute
+	intervalDuration := time.Duration(interval) * time.Second
+
+	start := time.Now()
+
+Loop:
+	for {
+		deployment, err = h.Kube().AppsV1().Deployments(namespace).Get(context.TODO(), deploymentName, metav1.GetOptions{})
+		elapsed := time.Since(start)
+
+		switch {
+		case err == nil:
+			// Success
+			break Loop
+		case strings.Contains(err.Error(), "forbidden"):
+			return nil, err
+		default:
+			if elapsed < timeoutDuration {
+				log.Printf("Waiting %v for %s deployment to exist", (timeoutDuration - elapsed), deploymentName)
+				time.Sleep(intervalDuration)
+			} else {
+				deployment = nil
+				err = fmt.Errorf("Failed to get %s Deployment before timeout", deploymentName)
+				break Loop
+			}
+		}
+	}
+
+	return deployment, err
+}
