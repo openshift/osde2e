796fa6de222f9442bda707eb1a204c35c52ff226
Alice Hubenko <ahubenko@redhat.com>
2020-10-20 18:13:24 -0700

    Added dedicated admin splunkforwarder test.

--- pkg/e2e/operators/splunkforwarder.go
+++ pkg/e2e/operators/splunkforwarder.go
@@ -1,9 +1,18 @@
 package operators
 
 import (
+	"context"
+
 	"github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 	"github.com/openshift/osde2e/pkg/common/alert"
 	"github.com/openshift/osde2e/pkg/common/helper"
+	sfv1alpha1 "github.com/openshift/splunk-forwarder-operator/pkg/apis/splunkforwarder/v1alpha1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
 var splunkForwarderBlocking string = "[Suite: operators] [OSD] Splunk Forwarder Operator"
@@ -18,7 +27,6 @@ var _ = ginkgo.Describe(splunkForwarderBlocking, func() {
 	var operatorNamespace string = "openshift-splunk-forwarder-operator"
 	var operatorLockFile string = "splunk-forwarder-operator-lock"
 	var defaultDesiredReplicas int32 = 1
-
 	var clusterRoleBindings = []string{
 		"splunk-forwarder-operator-clusterrolebinding",
 	}
@@ -39,4 +47,85 @@ var _ = ginkgo.Describe(splunkForwarderBlocking, func() {
 	checkUpgrade(helper.New(), "openshift-splunk-forwarder-operator",
 		"openshift-splunk-forwarder-operator", "splunk-forwarder-operator",
 		"splunk-forwarder-operator-catalog")
+
+	ginkgo.Context("splunkforwarders", func() {
+		ginkgo.It("dedicated admin should not be able to manage SplunkForwarders CR", func() {
+			name := "osde2e-dedicated-admin-splunkforwarder-x"
+			sf := makeMinimalSplunkforwarder("SplunkForwarder", "splunkforwarder.managed.openshift.io/v1alpha1", name)
+			err := dedicatedAaddSplunkforwarder(sf, h)
+			Expect(apierrors.IsForbidden(err)).To(BeTrue())
+			defer deleteSplunkforwarder(name, err, h)
+
+		})
+	})
+
+	ginkgo.Context("splunkforwarders", func() {
+		ginkgo.It("admin should be able to manage SplunkForwarders CR", func() {
+			name := "osde2e-splunkforwarder-test-2"
+			sf := makeMinimalSplunkforwarder("SplunkForwarder", "splunkforwarder.managed.openshift.io/v1alpha1", name)
+			err := addSplunkforwarder(sf, h)
+			Expect(err).NotTo(HaveOccurred())
+			defer deleteSplunkforwarder(name, err, h)
+
+		})
+	})
+
 })
+
+func makeMinimalSplunkforwarder(kind string, apiversion string, name string) sfv1alpha1.SplunkForwarder {
+	sf := sfv1alpha1.SplunkForwarder{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       kind,
+			APIVersion: apiversion,
+		},
+		ObjectMeta: metav1.ObjectMeta{
+
+			Name: name,
+		},
+		Spec: sfv1alpha1.SplunkForwarderSpec{
+			SplunkLicenseAccepted: true,
+			UseHeavyForwarder:     false,
+			SplunkInputs: []sfv1alpha1.SplunkForwarderInputs{
+				{
+					Path: "",
+				},
+			},
+		},
+	}
+	return sf
+}
+
+func dedicatedAaddSplunkforwarder(SplunkForwarder sfv1alpha1.SplunkForwarder, h *helper.H) error {
+	obj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(SplunkForwarder.DeepCopy())
+	if err != nil {
+		return err
+	}
+	unstructuredObj := unstructured.Unstructured{obj}
+	h.SetServiceAccount("system:serviceaccount:%s:dedicated-admin-project")
+	_, err = h.Dynamic().Resource(schema.GroupVersionResource{
+		Group: "splunkforwarder.managed.openshift.io", Version: "v1alpha1", Resource: "splunkforwarders",
+	}).Namespace("openshift-splunk-forwarder-operator").Create(context.TODO(), &unstructuredObj, metav1.CreateOptions{})
+	return (err)
+}
+
+func addSplunkforwarder(SplunkForwarder sfv1alpha1.SplunkForwarder, h *helper.H) error {
+	obj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(SplunkForwarder.DeepCopy())
+	if err != nil {
+		return err
+	}
+	unstructuredObj := unstructured.Unstructured{obj}
+	_, err = h.Dynamic().Resource(schema.GroupVersionResource{
+		Group: "splunkforwarder.managed.openshift.io", Version: "v1alpha1", Resource: "splunkforwarders",
+	}).Namespace(operatorNamespace).Create(context.TODO(), &unstructuredObj, metav1.CreateOptions{})
+	return (err)
+}
+
+func deleteSplunkforwarder(name string, err error, h *helper.H) error {
+	if err == nil {
+		e := h.Dynamic().Resource(schema.GroupVersionResource{
+			Group: "splunkforwarder.managed.openshift.io", Version: "v1alpha1", Resource: "splunkforwarders",
+		}).Namespace(operatorNamespace).Delete(context.TODO(), name, metav1.DeleteOptions{})
+		return (e)
+	}
+	return nil
+}
