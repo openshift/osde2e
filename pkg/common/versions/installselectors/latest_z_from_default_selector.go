f237471d6a21e9760a6997e88f701e6f88910df6
Jeffrey Sica <jeef111x@gmail.com>
2020-10-29 13:33:52 -0400

    add more granular y and z install selectors

---
+++ pkg/common/versions/installselectors/latest_z_from_default_selector.go
@@ -0,0 +1,65 @@
+package installselectors
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Masterminds/semver"
+	"github.com/openshift/osde2e/pkg/common/config"
+	"github.com/openshift/osde2e/pkg/common/spi"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	registerSelector(latestZVersion{})
+}
+
+// LatestVersion will always select the latest version of openshift.
+type latestZVersion struct{}
+
+func (l latestZVersion) ShouldUse() bool {
+	log.Printf("Should use latest z? %v", viper.GetBool(config.Cluster.LatestZReleaseAfterProdDefault))
+	return viper.GetBool(config.Cluster.LatestZReleaseAfterProdDefault)
+}
+
+func (l latestZVersion) Priority() int {
+	return 70
+}
+
+func (l latestZVersion) SelectVersion(versionList *spi.VersionList) (*semver.Version, string, error) {
+	availableVersions := versionList.AvailableVersions()
+	numVersions := len(availableVersions)
+	versionType := "latest z version from default"
+
+	if numVersions == 0 {
+		return nil, versionType, fmt.Errorf("not enough versions to select the latest version")
+	}
+
+	sortVersions(availableVersions)
+
+	var defaultVersion *spi.Version
+	var latestZVersion *spi.Version
+
+	for _, version := range availableVersions {
+		if version.Default() {
+			defaultVersion = version
+			latestZVersion = version
+		}
+
+		if defaultVersion == nil {
+			continue
+		}
+
+		if version.Version().Minor() == defaultVersion.Version().Minor() {
+			if version.Version().GreaterThan(latestZVersion.Version()) {
+				latestZVersion = version
+			}
+		}
+	}
+
+	if latestZVersion == nil || latestZVersion.Version().Equal(defaultVersion.Version()) {
+		return nil, versionType, fmt.Errorf("no version matching selector found")
+	}
+
+	return latestZVersion.Version(), versionType, nil
+}
