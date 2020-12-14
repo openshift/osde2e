ce06a68250e5142d408f79e5829b71534f502a09
agopala95 <email_id>
2020-12-14 10:41:36 -0500

    add healthcheck cmd to osde2ectl

---
+++ cmd/osde2ectl/healthcheck/cmd.go
@@ -0,0 +1,100 @@
+package healthcheck
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/openshift/osde2e/cmd/osde2e/common"
+	"github.com/openshift/osde2e/cmd/osde2e/helpers"
+	clusterutil "github.com/openshift/osde2e/pkg/common/cluster"
+	"github.com/openshift/osde2e/pkg/common/config"
+	"github.com/openshift/osde2e/pkg/common/providers/ocmprovider"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var Cmd = &cobra.Command{
+	Use:   "healthcheck",
+	Short: "Runs a healthcheck.",
+	Long:  "Runs a healthcheck on a cluster using the provided arguments.",
+	Args:  cobra.OnlyValidArgs,
+	RunE:  run,
+}
+
+var args struct {
+	configString    string
+	customConfig    string
+	secretLocations string
+	clusterID       string
+	environment     string
+	kubeConfig      string
+}
+
+func init() {
+	pfs := Cmd.PersistentFlags()
+	pfs.StringVar(
+		&args.configString,
+		"configs",
+		"",
+		"A comma separated list of built in configs to use",
+	)
+	Cmd.RegisterFlagCompletionFunc("configs", helpers.ConfigComplete)
+	pfs.StringVar(
+		&args.customConfig,
+		"custom-config",
+		"",
+		"Custom config file for osde2ectl",
+	)
+	pfs.StringVar(
+		&args.secretLocations,
+		"secret-locations",
+		"",
+		"A comma separated list of possible secret directory locations for loading secret configs.",
+	)
+	pfs.StringVarP(
+		&args.clusterID,
+		"cluster-id",
+		"i",
+		"",
+		"Existing OCM cluster ID to run healthchecks against.",
+	)
+	pfs.StringVarP(
+		&args.environment,
+		"environment",
+		"e",
+		"",
+		"Cluster provider environment to use.",
+	)
+
+	pfs.StringVarP(
+		&args.kubeConfig,
+		"kube-config",
+		"k",
+		"",
+		"Path to local Kube config for running healthchecks against.",
+	)
+
+	viper.BindPFlag(config.Cluster.ID, Cmd.PersistentFlags().Lookup("cluster-id"))
+	viper.BindPFlag(ocmprovider.Env, Cmd.PersistentFlags().Lookup("environment"))
+	viper.BindPFlag(config.Kubeconfig.Path, Cmd.PersistentFlags().Lookup("kube-config"))
+}
+
+func run(cmd *cobra.Command, argv []string) error {
+	if err := common.LoadConfigs(args.configString, args.customConfig, args.secretLocations); err != nil {
+		return fmt.Errorf("error loading initial state: %v", err)
+	}
+	logger := log.New(os.Stdout, "", log.LstdFlags)
+	isHealthy, failures, _ := clusterutil.PollClusterHealth(args.clusterID, logger)
+	if isHealthy {
+		log.Printf("Cluster %s is healthy!\n", args.clusterID)
+	} else {
+		log.Printf("Cluster %s is not healthy yet.\n", args.clusterID)
+		if len(failures) > 0 {
+			log.Printf("Currently failing %s health checks", strings.Join(failures, ", "))
+		}
+		return fmt.Errorf("healthcheck failed")
+	}
+	return nil
+}
