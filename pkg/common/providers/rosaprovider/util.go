7afbe944d3414a3229f80a38da2db155da2bae61
Jeffrey Sica <jeef111x@gmail.com>
2021-01-13 09:14:06 -0500

    wrap rosa cluster creation with better error handling

--- pkg/common/providers/rosaprovider/util.go
+++ pkg/common/providers/rosaprovider/util.go
@@ -1,6 +1,8 @@
 package rosaprovider
 
 import (
+	"fmt"
+	"log"
 	"os"
 	"strings"
 
@@ -9,7 +11,7 @@ import (
 
 // At the moment, rosa requires AWS sessions to be set globally. To get around that, we'll use this
 // helper method here so that we can set environment variables and restore them before returning from the function.
-func callAndSetAWSSession(f func()) {
+func callAndSetAWSSession(f func() error) error {
 	var env []string
 	defer func() {
 		os.Clearenv()
@@ -23,6 +25,23 @@ func callAndSetAWSSession(f func()) {
 	os.Setenv("AWS_ACCESS_KEY_ID", viper.GetString(AWSAccessKeyID))
 	os.Setenv("AWS_SECRET_ACCESS_KEY", viper.GetString(AWSSecretAccessKey))
 	os.Setenv("AWS_REGION", viper.GetString(AWSRegion))
+	error := false
+	if os.Getenv("AWS_ACCESS_KEY_ID") == "" {
+		log.Println("AWS_ACCESS_KEY_ID is empty")
+		error = true
+	}
+	if os.Getenv("AWS_SECRET_ACCESS_KEY") == "" {
+		log.Println("AWS_SECRET_ACCESS_KEY is empty")
+		error = true
+	}
+	if os.Getenv("AWS_REGION") == "" {
+		log.Println("AWS_REGION is empty")
+		error = true
+	}
 
-	f()
+	if !error {
+		return f()
+	}
+
+	return fmt.Errorf("one or more required AWS variables were not set")
 }
