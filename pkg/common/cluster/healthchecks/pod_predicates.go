b5c3261cc6d03a3e07784f082fa134297aac4d00
Eric Fried <efried@redhat.com>
2021-02-01 19:21:44 -0600

    Enable osd-cluster-ready in-cluster healthcheck
    This commit enables real health checks to be run from the
    [osd-cluster-ready Job](https://github.com/iamkirkbater/osd-cluster-ready-job) by:
    - Tweaking `PollClusterHealth` to use in-cluster REST config if the
    `clusterID` is not specified (empty string).
    - Ignoring the `osd-cluster-ready` pod itself, as it will be Pending while
    it's running the health check.

--- pkg/common/cluster/healthchecks/pod_predicates.go
+++ pkg/common/cluster/healthchecks/pod_predicates.go
@@ -8,6 +8,13 @@ func IsClusterPod(pod kubev1.Pod) bool {
 	return containsPrefixes(pod.Namespace, "openshift-", "kube-", "redhat-")
 }
 
+// IsNotReadinessPod ignores the chicken/egg situation where we're running health checks
+// from within an ephemeral osd-cluster-ready-* Pod. That Pod would otherwise fail the
+// health check it is running because it's in Pending state.
+func IsNotReadinessPod(pod kubev1.Pod) bool {
+	return !matchingNamePrefix(pod, "osd-cluster-ready-")
+}
+
 func MatchesNames(name ...string) PodPredicate {
 	return func(p kubev1.Pod) bool {
 		return matchingNamePrefix(p, name...)
