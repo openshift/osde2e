c3ce8a027c12edcde25f5ef3c44ee816a44746a0
Sam Nguyen <khnguyen@redhat.com>
2021-02-10 13:24:12 -0500

    Add dedicated-admin permission checks

--- pkg/e2e/osd/dedicatedadmin.go
+++ pkg/e2e/osd/dedicatedadmin.go
@@ -5,10 +5,13 @@ import (
 
 	"github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	projectv1 "github.com/openshift/api/project/v1"
+	userv1 "github.com/openshift/api/user/v1"
 	"github.com/openshift/osde2e/pkg/common/alert"
 	"github.com/openshift/osde2e/pkg/common/config"
 	"github.com/openshift/osde2e/pkg/common/helper"
 	"github.com/spf13/viper"
+	rbacv1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/rest"
 )
@@ -70,5 +73,195 @@ var _ = ginkgo.Describe(dedicatedAdminTestName, func() {
 			Expect(err).To(HaveOccurred())
 
 		}, float64(viper.GetFloat64(config.Tests.PollingTimeout)))
+
+		// dedicated-admin SA can create projectrequest object
+		ginkgo.It("ded-admin SA can create projectrequest", func() {
+
+			// Impersonate ded-admin
+			h.Impersonate(rest.ImpersonationConfig{
+				UserName: "dummy-admin@redhat.com",
+				Groups: []string{
+					"system:serviceaccounts:dedicated-admin",
+				},
+			})
+			defer func() {
+				h.Impersonate(rest.ImpersonationConfig{})
+			}()
+
+			proj := &projectv1.ProjectRequest{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "osde2e-sample-cust-proj",
+				},
+				DisplayName: "osde2e-sample-cust-proj",
+			}
+			_, err := h.Project().ProjectV1().ProjectRequests().Create(context.TODO(), proj, metav1.CreateOptions{})
+			Expect(err).NotTo(HaveOccurred())
+
+		}, float64(viper.GetFloat64(config.Tests.PollingTimeout)))
+
+		// regular dedicated-admin user can create 'admin' rolebinding
+		ginkgo.It("ded-admin user can create 'admin' rolebinding", func() {
+
+			// Impersonate ded-admin
+			h.Impersonate(rest.ImpersonationConfig{
+				UserName: "dummy-admin@redhat.com",
+				Groups: []string{
+					"dedicated-admins",
+				},
+			})
+			defer func() {
+				h.Impersonate(rest.ImpersonationConfig{})
+			}()
+
+			// create dummy user to attach to 'admin'
+			user := &userv1.User{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "dummy-user@redhat.com",
+				},
+			}
+
+			dummyNs := "osde2e-sample-cust-proj"
+			dummyKind := "ClusterRole"
+			dummyKindName := "admin"
+			_, err := createRolebinding(dummyNs, user, dummyKind, dummyKindName, h)
+			Expect(err).NotTo(HaveOccurred())
+
+		}, float64(viper.GetFloat64(config.Tests.PollingTimeout)))
+
+		// regular dedicated-admin user can create 'edit' rolebinding
+		ginkgo.It("ded-admin user can create edit rolebinding", func() {
+
+			// Impersonate ded-admin
+			h.Impersonate(rest.ImpersonationConfig{
+				UserName: "dummy-admin@redhat.com",
+				Groups: []string{
+					"dedicated-admins",
+				},
+			})
+			defer func() {
+				h.Impersonate(rest.ImpersonationConfig{})
+			}()
+
+			// create dummy user to attach to 'admin'
+			user := &userv1.User{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "dummy-user@redhat.com",
+				},
+			}
+
+			dummyNs := "osde2e-sample-cust-proj"
+			dummyKind := "ClusterRole"
+			dummyKindName := "edit"
+			_, err := createRolebinding(dummyNs, user, dummyKind, dummyKindName, h)
+			Expect(err).NotTo(HaveOccurred())
+
+		}, float64(viper.GetFloat64(config.Tests.PollingTimeout)))
+
+		// dedicated-admin SA can create 'edit' rolebinding
+		ginkgo.It("ded-admin SA can create 'edit' rolebinding", func() {
+
+			// Impersonate ded-admin
+			h.Impersonate(rest.ImpersonationConfig{
+				UserName: "dummy-admin@redhat.com",
+				Groups: []string{
+					"system:serviceaccounts:dedicated-admin",
+				},
+			})
+			defer func() {
+				h.Impersonate(rest.ImpersonationConfig{})
+			}()
+
+			// create dummy user to attach to 'admin'
+			user := &userv1.User{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "dummy-user@redhat.com",
+				},
+			}
+
+			dummyNs := "osde2e-sample-cust-proj"
+			dummyKind := "ClusterRole"
+			dummyKindName := "edit"
+			_, err := createRolebinding(dummyNs, user, dummyKind, dummyKindName, h)
+			Expect(err).NotTo(HaveOccurred())
+
+		}, float64(viper.GetFloat64(config.Tests.PollingTimeout)))
+
+		// dedicated-admin SA can create 'admin' rolebinding
+		ginkgo.It("ded-admin SA can create 'admin' rolebinding", func() {
+
+			// Impersonate ded-admin
+			h.Impersonate(rest.ImpersonationConfig{
+				UserName: "dummy-admin@redhat.com",
+				Groups: []string{
+					"system:serviceaccounts:dedicated-admin",
+				},
+			})
+			defer func() {
+				h.Impersonate(rest.ImpersonationConfig{})
+			}()
+
+			// create dummy user to attach to 'admin'
+			user := &userv1.User{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "dummy-user@redhat.com",
+				},
+			}
+
+			dummyNs := "osde2e-sample-cust-proj"
+			dummyKind := "ClusterRole"
+			dummyKindName := "admin"
+			_, err := createRolebinding(dummyNs, user, dummyKind, dummyKindName, h)
+			Expect(err).NotTo(HaveOccurred())
+
+		}, float64(viper.GetFloat64(config.Tests.PollingTimeout)))
+
+		// dedicated-admin SA can delete project
+		ginkgo.It("ded-admin SA can delete project", func() {
+
+			// Impersonate ded-admin
+			h.Impersonate(rest.ImpersonationConfig{
+				UserName: "dummy-admin@redhat.com",
+				Groups: []string{
+					"system:serviceaccounts:dedicated-admin",
+				},
+			})
+			defer func() {
+				h.Impersonate(rest.ImpersonationConfig{})
+			}()
+
+			proj := &projectv1.ProjectRequest{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "osde2e-sample-cust-proj",
+				},
+				DisplayName: "osde2e-sample-cust-proj",
+			}
+			err := h.Project().ProjectV1().Projects().Delete(context.TODO(), proj.Name, metav1.DeleteOptions{})
+			Expect(err).NotTo(HaveOccurred())
+
+		}, float64(viper.GetFloat64(config.Tests.PollingTimeout)))
+
 	})
 })
+
+// createRolebinding takes in the desired namespace, user, and roleRef kind and kindName
+// returns the corresponding rolebinding created on cluster
+func createRolebinding(ns string, user *userv1.User, kind string, kindName string, h *helper.H) (*rbacv1.RoleBinding, error) {
+
+	rb, err := h.Kube().RbacV1().RoleBindings(ns).Create(context.TODO(), &rbacv1.RoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: "osde2e-admin-rolebind",
+		},
+		Subjects: []rbacv1.Subject{
+			{
+				Kind: rbacv1.UserKind,
+				Name: user.Name,
+			},
+		},
+		RoleRef: rbacv1.RoleRef{
+			APIGroup: rbacv1.GroupName,
+			Kind:     kind,
+			Name:     kindName,
+		},
+	}, metav1.CreateOptions{})
+	return rb, err
+}
