d0341453b5a1b9cb5265761e26bb2e02fabc4fc9
Jeffrey Sica <jeef111x@gmail.com>
2021-02-12 13:44:55 -0500

    update adoption script with cluster provisioning

--- ci/prow-ocm-adoption.sh
+++ ci/prow-ocm-adoption.sh
@@ -2,24 +2,52 @@
 
 set -o pipefail
 
-export OCM_CONFIG=$(pwd)/.ocm.json
+mkdir -p installer
 
-{
+export VERSION=latest
+export RELEASE_IMAGE=$(curl -s https://mirror.openshift.com/pub/openshift-v4/clients/ocp/$VERSION/release.txt | grep 'Pull From: quay.io' | awk -F ' ' '{print $3}')
 
-go get -u github.com/openshift-online/ocm-cli/cmd/ocm
+export cmd=openshift-install
+export pullsecret_file=~/Downloads/crc-pull-secret.txt
+export extract_dir=$(pwd)
 
 if [ -z "$OCM_TOKEN" ]; then
     echo "Assuming the token should be read from Prow";
     export OCM_TOKEN=$(cat /usr/local/osde2e-credentials/ocm-refresh-token)
 fi
 
+if [ -z "AWS_ACCESS_KEY_ID" ]; then
+    export AWS_ACCESS_KEY_ID=$(cat /usr/local/osde2e-credentials/aws-access-key-id)
+fi
+
+if [ -z "AWS_SECRET_ACCESS_KEY" ]; then
+    export AWS_SECRET_ACCESS_KEY=$(cat /usr/local/osde2e-credentials/aws-secret-access-key)
+fi
+
+
+cp /usr/local/osde2e-credentials/stage-installer-config ./installer/installer-config.yaml
+
+
+
+curl -s https://mirror.openshift.com/pub/openshift-v4/clients/ocp/$VERSION/openshift-client-linux.tar.gz | tar zxvf - oc
+
+./oc adm release extract --registry-config "${pullsecret_file}" --command=$cmd --to "${extract_dir}" ${RELEASE_IMAGE}
+
+./openshift-install create cluster --dir=$(pwd)/installer/ --log-level info
+
+export OCM_CONFIG=$(pwd)/.ocm.json
+export KUBECONFIG=$(pwd)/installer/auth/kubeconfig
+
+{
+
+go get -u github.com/openshift-online/ocm-cli/cmd/ocm
+
 if [ -z "$OCM_URL" ]; then
     ocm login --token=$OCM_TOKEN
 else 
     ocm login --url=$OCM_URL --token=$OCM_TOKEN
-    echo "Old Config: "
-    oc get -n openshift-monitoring configmap/cluster-monitoring-config -o yaml
-    cat <<EOF | oc apply -f -
+
+    cat <<EOF | oc create -n openshift-monitoring -f -
 apiVersion: v1
 kind: ConfigMap
 metadata:
@@ -27,30 +55,12 @@ metadata:
   namespace: openshift-monitoring
 data:
   config.yaml: |
-    prometheusK8s:
-      retention: 15d
-      volumeClaimTemplate:
-        metadata:
-          name: prometheus-data
-        spec:
-          storageClassName: gp2
-          resources:
-            requests:
-              storage: 50Gi
-    alertmanagerMain:
-      volumeClaimTemplate:
-        metadata:
-          name: alertmanager-data
-        spec:
-          storageClassName: gp2
-          resources:
-            requests:
-              storage: 10Gi
     telemeterClient:
       telemeterServerURL: https://infogw.api.stage.openshift.com
+
 EOF
     sleep 600;
-    echo "New config"
+    echo "Output new config"
     oc get -n openshift-monitoring configmap/cluster-monitoring-config -o yaml
 fi
 
