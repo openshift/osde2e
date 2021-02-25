f2ab5c749e4ff96ac7d040ac89b3ea9128d4964b
Chris Waldon <cwaldon@redhat.com>
2021-02-25 08:21:06 -0500

    feat: switch to pagerduty events v2 api
    Signed-off-by: Chris Waldon <cwaldon@redhat.com>

--- pkg/common/pagerduty/pagerduty.go
+++ pkg/common/pagerduty/pagerduty.go
@@ -6,60 +6,26 @@ import (
 	pd "github.com/PagerDuty/go-pagerduty"
 )
 
-// Config holds state necessary to open pagerduty incidents against a particular
-// service and associated with a particular escalation policy.
+// Config holds state necessary to open pagerduty incidents using an integration key.
 type Config struct {
-	Token, ServiceID, PolicyID string
+	IntegrationKey string
 }
 
-// FireIncident attempts to create an incident with the provided title and body that
-// is associated with the service and policy IDs stored in the PDConfig
-func (pdc Config) FireIncident(title, body string) error {
-	client := pd.NewClient(pdc.Token)
-	service, err := pdService(client, pdc.ServiceID)
-	if err != nil {
-		return err
-	}
-	policy, err := pdPolicy(client, pdc.PolicyID)
-	if err != nil {
-		return err
-	}
-	_, err = client.CreateIncident(title, &pd.CreateIncidentOptions{
-		Title: title,
-		Type:  "incident",
-		Body: &pd.APIDetails{
-			Type:    "incident_body",
-			Details: body,
+// FireIncident attempts to create an alert indicating a failure in the provided
+// pipeline.
+func (pdc Config) FireAlert(pipeline, details string) error {
+	event := pd.V2Event{
+		RoutingKey: pdc.IntegrationKey,
+		Action:     "trigger",
+		Payload: &pd.V2Payload{
+			Summary:  pipeline + " failed",
+			Severity: "info",
+			Source:   pipeline,
 		},
-		Urgency:          "low",
-		Service:          &service,
-		EscalationPolicy: &policy,
-	})
+	}
+	_, err := pd.ManageEvent(event)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed firing alert: %w", err)
 	}
 	return nil
-}
-
-func pdService(client *pd.Client, id string) (pd.APIReference, error) {
-	s, err := client.GetService(id, nil)
-	if err != nil {
-		return pd.APIReference{}, fmt.Errorf("failed looking up service %s: %w", id, err)
-	}
-	return pdRef(s.APIObject), nil
-}
-
-func pdPolicy(client *pd.Client, id string) (pd.APIReference, error) {
-	p, err := client.GetEscalationPolicy(id, nil)
-	if err != nil {
-		return pd.APIReference{}, fmt.Errorf("failed looking up escalation policy %s: %w", id, err)
-	}
-	return pdRef(p.APIObject), nil
-}
-
-func pdRef(obj pd.APIObject) pd.APIReference {
-	return pd.APIReference{
-		ID:   obj.ID,
-		Type: obj.Type,
-	}
 }
