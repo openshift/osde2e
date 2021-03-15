b90b804fe929bb68534f17b61d04a9149ff48406
Ron Green <11993626+georgettica@users.noreply.github.com>
2021-03-15 16:05:42 +0200

    feat(rmo): add osde2e tests
    - initial work here
    - the internal 'make test-integration' is not moved here fully for now
    - a Q is raised inside the file regarding an odd choice
    
    - feat(rmo): add integration tests
    
    - go mod updated to accomodate for the new operator dependency
    - add common crud.go for a consistent place for common resources
    - pull out user-workload-monitoring components as they are used in
      routemonitor aswell
    - add `ginkgo.By` to seperate a huge test`
    
    fix: change image
    - change error response to not include UWM
    
    Thanks also to:
    Co-authored-by: Chris Waldon <cwaldon@redhat.com>
    Co-authored-by: Manuel Dewald <github@manueldewald.de>

--- pkg/e2e/verify/user-workload-monitoring.go
+++ pkg/e2e/verify/user-workload-monitoring.go
@@ -39,20 +39,21 @@ var _ = ginkgo.Describe(userWorkloadMonitoringTestName, func() {
 	ginkgo.Context("User Workload Monitoring", func() {
 		ginkgo.It("has the required prerequisites for testing", func() {
 			//Create a new user that will have dedicated-admin privileges, add the user to the dedicated-admins group
-			_, err := createUser(userName, identities, groups, h)
+			_, err := helper.CreateUser(userName, identities, groups, h)
 			Expect(err).NotTo(HaveOccurred(), "Could not create user workload monitoring testing user")
 			//Add the user to the dedicated-admin group
-			_, err = addUserToGroup(userName, groups[0], h)
+			_, err = helper.AddUserToGroup(userName, groups[0], h)
 			Expect(err).NotTo(HaveOccurred(), "Could not grant dedicated-admin permissions to user workload monitoring user")
 			//Create a namespace to run the tests in
-			_, err = createNamespace(uwmtestns, h)
+			_, err = helper.CreateNamespace(uwmtestns, h)
 			Expect(err).NotTo(HaveOccurred(), "Could not create user workload monitoring testing namespace")
 			//Launch a pod & service as the targets of the ServiceMonitor and PrometheusRules objects
-			pod := newPod(prometheusName, uwmtestns, "quay.io/brancz/prometheus-example-app:v0.2.0")
-			err = createPodUwm(pod, uwmtestns, h)
+			pod := helper.SamplePod(prometheusName, uwmtestns, "quay.io/brancz/prometheus-example-app:v0.2.0")
+			err = helper.CreatePod(pod, uwmtestns, h)
 			Expect(err).NotTo(HaveOccurred(), "Could not create user workload monitoring testing pod")
-			svc := GenerateService(8080, prometheusName, uwmtestns, prometheusName)
-			err = createService(svc, h)
+
+			svc := helper.SampleService(8080, 8080, prometheusName, uwmtestns, prometheusName)
+			err = helper.CreateService(svc, h)
 			Expect(err).NotTo(HaveOccurred(), "Could not create user workload monitoring testing service")
 		})
 
@@ -126,59 +127,6 @@ var _ = ginkgo.Describe(userWorkloadMonitoringTestName, func() {
 
 })
 
-func createService(svc *kv1.Service, h *helper.H) error {
-	uwm, err := h.Kube().CoreV1().Services(svc.Namespace).Create(context.TODO(), svc, metav1.CreateOptions{})
-	log.Printf("Result of the create command: (%v)", err)
-	if err != nil {
-		log.Printf("Could not issue create command")
-		return err
-	}
-
-	// Wait for the pod to create.
-	err = wait.PollImmediate(5*time.Second, 1*time.Minute, func() (bool, error) {
-		if _, err := h.Kube().CoreV1().Services(uwm.Namespace).Get(context.TODO(), uwm.Name, metav1.GetOptions{}); err != nil {
-			return false, nil
-		}
-		return true, nil
-	})
-	return err
-}
-
-func createPodUwm(pod *kv1.Pod, namespace string, h *helper.H) error {
-	uwm, err := h.Kube().CoreV1().Pods(namespace).Create(context.TODO(), pod, metav1.CreateOptions{})
-	log.Printf("Result of the create command: (%v)", err)
-	if err != nil {
-		log.Printf("Could not issue create command")
-		return err
-	}
-
-	// Wait for the pod to create.
-	err = wait.PollImmediate(5*time.Second, 1*time.Minute, func() (bool, error) {
-		if _, err := h.Kube().CoreV1().Pods(namespace).Get(context.TODO(), uwm.Name, metav1.GetOptions{}); err != nil {
-			return false, nil
-		}
-		return true, nil
-	})
-	return err
-}
-
-func newPod(name, namespace, imageName string) *kv1.Pod {
-	return &kv1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      name,
-			Namespace: namespace,
-		},
-		Spec: kv1.PodSpec{
-			Containers: []kv1.Container{
-				{
-					Name:  "test",
-					Image: imageName,
-				},
-			},
-		},
-	}
-}
-
 func newServiceMonitor(name, namespace string) *v1.ServiceMonitor {
 	return &v1.ServiceMonitor{
 		TypeMeta: metav1.TypeMeta{},
@@ -197,32 +145,6 @@ func newServiceMonitor(name, namespace string) *v1.ServiceMonitor {
 			TargetLimit:       0,
 		},
 	}
-}
-
-func GenerateService(port int32, serviceName, serviceNamespace string, prometheusName string) *kv1.Service {
-	service := &kv1.Service{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      prometheusName,
-			Namespace: serviceNamespace,
-			Labels:    map[string]string{prometheusName: serviceName},
-		},
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Service",
-			APIVersion: "v1",
-		},
-		Spec: kv1.ServiceSpec{
-			Ports: []kv1.ServicePort{
-				{
-					Port:       8080,
-					Protocol:   kv1.ProtocolTCP,
-					TargetPort: intstr.IntOrString{IntVal: 8080},
-					Name:       "web",
-				},
-			},
-			Selector: map[string]string{prometheusName: serviceName},
-		},
-	}
-	return service
 }
 
 func newPrometheusRule(name, namespace string) *v1.PrometheusRule {
