ff977251b1b7cdaec450aca087dc35969210b035
agopala95 <email_id>
2021-04-23 01:30:17 -0400

    prometheus pod check in collect prometheus data test

--- pkg/e2e/state/prometheus.go
+++ pkg/e2e/state/prometheus.go
@@ -1,11 +1,16 @@
 package state
 
 import (
+	"errors"
+	"time"
+
 	"github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
 	"github.com/openshift/osde2e/pkg/common/helper"
 	"github.com/openshift/osde2e/pkg/common/runner"
+	"github.com/openshift/osde2e/pkg/e2e/verify"
+	"k8s.io/apimachinery/pkg/util/wait"
 )
 
 const (
@@ -25,6 +30,27 @@ var _ = ginkgo.Describe(clusterStateInformingName, func() {
 		// 1 as tar 1.26 will exit 1 if files change while the tar is running, as is
 		// common for a running prometheus instance
 		cmd := promCollectCmd + " > " + runner.DefaultRunner.OutputDir + "/prometheus.tar.gz; err=$? ; if (( $err != 0 )) ; then exit $err ; fi"
+
+		poderr := wait.PollImmediate(2*time.Second, 5*time.Minute, func() (bool, error) {
+			podCount := 0
+			var podErr error
+			list, _ := verify.FilterPods("openshift-monitoring", "app=prometheus", h)
+			names, podNum := verify.GetPodNames("prometheus-operator", list, h)
+			if podNum > 0 {
+				for _, value := range names {
+					if (value == "prometheus-k8s-0") || (value == "prometheus-k8s-1") {
+						podCount += 1
+					}
+				}
+				if podCount == 2 {
+					podErr = nil
+					return true, podErr
+				}
+			}
+			podErr = errors.New("Prometheus pods are not in running state")
+			return false, podErr
+		})
+		Expect(poderr).NotTo(HaveOccurred())
 
 		h.SetServiceAccount("system:serviceaccount:%s:cluster-admin")
 		r := h.Runner(cmd)
