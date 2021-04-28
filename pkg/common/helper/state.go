3fb1373f4f01983d07220482a079158af847f37a
Chris Waldon <cwaldon@redhat.com>
2021-04-28 14:25:58 -0400

    fix: use plural resource name to actually get results
    Signed-off-by: Chris Waldon <cwaldon@redhat.com>

--- pkg/common/helper/state.go
+++ pkg/common/helper/state.go
@@ -59,7 +59,7 @@ var (
 		{Group: "rbac.authorization.k8s.io", Version: "v1", Resource: "roles"},
 
 		// machine
-		{Group: "machine.openshift.io", Version: "v1", Resource: "machine"},
+		{Group: "machine.openshift.io", Version: "v1beta1", Resource: "machines"},
 
 		// core
 		{Group: "", Version: "v1", Resource: "configmaps"},
