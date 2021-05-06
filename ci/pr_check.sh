0c678ad0c17f22f1a5eb0704247ea3bc7ce144f6
Jeffrey Sica <jeef111x@gmail.com>
2021-05-06 10:00:15 -0400

    restore pr_check.sh, move logic to prow_pr_check

--- ci/pr_check.sh
+++ ci/pr_check.sh
@@ -1,7 +1,3 @@
 #!/bin/bash -e
 
-make build
-
-CLUSTER_ID=1sAHFzPxMgXQoqjDErH7CTN2NBb \
-OCM_TOKEN=$(cat /usr/local/osde2e-credentials/ocm-token) \
-./out/osde2e --configs=prod,aws
+make build-image
--- ci/prow_pr_check.sh
+++ ci/prow_pr_check.sh
@@ -7,4 +7,10 @@ set -o pipefail
 {
     export GOFLAGS=""
     make check
+
+    make build
+
+    CLUSTER_ID=1kgq75e84eloshd81c8gcjl10fglg0jb \
+    ./out/osde2e test --configs=prod,aws --secret-locations=/usr/local/osde2e-common,/usr/local/osde2e-credentials
+
 } 2>&1 | tee -a "$REPORT_DIR/test_output.log"
