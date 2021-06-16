882244e451484fc8ffa796afe86482314fe22728
Jeffrey Sica <jeef111x@gmail.com>
2021-06-16 13:26:58 -0400

    wait for project to exist before moving on from project creation

--- pkg/common/helper/projects.go
+++ pkg/common/helper/projects.go
@@ -3,11 +3,13 @@ package helper
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/openshift/osde2e/pkg/common/runner"
 
 	projectv1 "github.com/openshift/api/project/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
 )
 
 func (h *H) createProject(suffix string) (*projectv1.Project, error) {
@@ -16,7 +18,27 @@ func (h *H) createProject(suffix string) (*projectv1.Project, error) {
 			Name: "osde2e-" + suffix,
 		},
 	}
-	return h.Project().ProjectV1().Projects().Create(context.TODO(), proj, metav1.CreateOptions{})
+
+	project, err := h.Project().ProjectV1().Projects().Create(context.TODO(), proj, metav1.CreateOptions{})
+
+	if err != nil {
+		return project, err
+	}
+
+	wait.PollImmediate(5*time.Second, 60*time.Second, func() (done bool, err error) {
+		project, err = h.Project().ProjectV1().Projects().Get(context.TODO(), project.Name, metav1.GetOptions{})
+		if err != nil {
+			return false, err
+		}
+
+		if project != nil {
+			return true, nil
+		}
+
+		return false, nil
+	})
+
+	return project, err
 }
 
 func (h *H) inspect(projectName string) error {
