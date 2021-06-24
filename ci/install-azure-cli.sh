26e5188e2e92987ebb5cb93e172ddc0ab2e958aa
Jeffrey Sica <jeef111x@gmail.com>
2021-06-24 14:53:36 -0400

    update to work within release:golang-1.16 image (theoretically)

--- ci/install-azure-cli.sh
+++ ci/install-azure-cli.sh
@@ -4,13 +4,13 @@ set -euo pipefail
 
 echo "Installing Azure CLI"
 
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
 echo -e "[azure-cli]
 name=Azure CLI
 baseurl=https://packages.microsoft.com/yumrepos/azure-cli
 enabled=1
 gpgcheck=1
-gpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/azure-cli.repo
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/azure-cli.repo
 
-sudo dnf install azure-cli
+yum install -y azure-cli
