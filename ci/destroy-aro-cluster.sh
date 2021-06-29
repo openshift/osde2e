5b742600f6623205e0a81a64756738b20cd95590
Jeffrey Sica <jeef111x@gmail.com>
2021-06-29 11:22:47 -0400

    remove other az cli install script reference

--- ci/destroy-aro-cluster.sh
+++ ci/destroy-aro-cluster.sh
@@ -2,8 +2,6 @@
 
 set -euo pipefail
 
-source ./ci/install-azure-cli.sh
-
 echo "Logging into Azure"
 
 az login --service-principal --username "$(cat /usr/local/osde2e-credentials/aro-app-id)" --password "$(cat /usr/local/osde2e-credentials/aro-password)" --tenant "$(cat /usr/local/osde2e-credentials/aro-tenant)"
