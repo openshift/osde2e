5fa9ca1ebe3079da151177ad6a3fbeb9d473356d
Jeffrey Sica <jeef111x@gmail.com>
2021-07-16 10:39:01 -0400

    add cleanup script that deletes all osde2e clusters

---
+++ ci/cleanup-aro-clusters.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "Logging into Azure"
+
+az login --service-principal --username "$(cat /usr/local/osde2e-credentials/aro-app-id)" --password "$(cat /usr/local/osde2e-credentials/aro-password)" --tenant "$(cat /usr/local/osde2e-credentials/aro-tenant)"
+
+az aro list | jq '.[] | select(.name|test("osde2e-.")) | .name' | while read line ; do
+    echo "Cleaning up old cluster cluster $line"
+
+    az aro delete --yes --name="$line" --resource-group="$(cat "$line")"
+    az group delete --yes --name="$(cat "$line")"
+done
