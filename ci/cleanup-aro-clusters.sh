fe15e4662fc585a315808c66a91cac026099eac2
Jeffrey Sica <jeef111x@gmail.com>
2021-07-28 12:15:13 -0500

    fix aro cleanup script to remove quotes from cluster names

--- ci/cleanup-aro-clusters.sh
+++ ci/cleanup-aro-clusters.sh
@@ -7,8 +7,10 @@ echo "Logging into Azure"
 az login --service-principal --username "$(cat /usr/local/osde2e-credentials/aro-app-id)" --password "$(cat /usr/local/osde2e-credentials/aro-password)" --tenant "$(cat /usr/local/osde2e-credentials/aro-tenant)"
 
 az aro list | jq '.[] | select(.name|test("osde2e-.")) | .name' | while read line ; do
-    echo "Cleaning up old cluster cluster $line"
+    cluster_id=$(echo "$line" | sed -e 's/^"//' -e 's/"$//')
 
-    az aro delete --yes --name="$line" --resource-group="$line"
-    az group delete --yes --name="$line"
+    echo "Cleaning up old cluster cluster $cluster_id"
+
+    az aro delete --yes --name="$cluster_id" --resource-group="$cluster_id"
+    az group delete --yes --name="$cluster_id"
 done
