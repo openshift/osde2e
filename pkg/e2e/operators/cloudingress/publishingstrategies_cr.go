35a8c674d5bd4ee55f1b724a444b832aba01baab
Linh Nguyen <linnguye@linnguye.bos.csb>
2021-10-28 13:37:30 -0400

    remove cloud-ingress-operator annotation check

--- pkg/e2e/operators/cloudingress/publishingstrategies_cr.go
+++ pkg/e2e/operators/cloudingress/publishingstrategies_cr.go
@@ -39,8 +39,8 @@ var _ = ginkgo.Describe(constants.SuiteOperators+TestPrefix, func() {
 			err := addPublishingstrategy(h, ps)
 			Expect(apierrors.IsForbidden(err)).To(BeTrue())
 
-			ingress, _ := getingressController(h, "default")
-			Expect(ingress.Annotations["Owner"]).To(Equal("cloud-ingress-operator"))
+			//ingress, _ := getingressController(h, "default")
+			//Expect(ingress.Annotations["Owner"]).To(Equal("cloud-ingress-operator"))
 		})
 
 		ginkgo.It("cluster admin should be allowed to manage publishingstrategies CR", func() {
@@ -52,8 +52,8 @@ var _ = ginkgo.Describe(constants.SuiteOperators+TestPrefix, func() {
 			}()
 			Expect(err).NotTo(HaveOccurred())
 			//check the annotation for owned ingress
-			ingress, _ := getingressController(h, "default")
-			Expect(ingress.Annotations["Owner"]).To(Equal("cloud-ingress-operator"))
+			//ingress, _ := getingressController(h, "default")
+			//Expect(ingress.Annotations["Owner"]).To(Equal("cloud-ingress-operator"))
 		})
 
 	})
