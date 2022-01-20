c85084bc52f5c57822c37df863aaf831e9477f31
Diego Santamaria <dsantama@redhat.com>
2022-01-20 15:04:28 -0700

    Made requested changes.

--- pkg/common/load/load.go
+++ pkg/common/load/load.go
@@ -71,7 +71,7 @@ func Configs(configs []string, customConfig string, secretLocations []string) er
 			passthruSecrets := viper.GetStringMapString(config.NonOSDe2eSecrets)
 			if !strings.Contains(folder, "osde2e-credentials") && !strings.Contains(folder, "osde2e-common") {
 				if viper.Get(config.Addons.IDs) != nil {
-					passthruSecrets["ocm-token-refresh"] = viper.Get("ocm.token").(string)
+					passthruSecrets["ocm-token-refresh"] = viper.GetString("ocm.token")
 				} else {
 					continue
 				}
@@ -81,13 +81,12 @@ func Configs(configs []string, customConfig string, secretLocations []string) er
 				if err != nil {
 					return fmt.Errorf("Error walking folder %s: %s", folder, err.Error())
 				}
-				fmt.Printf(path)
 
 				data, err := ioutil.ReadFile(path)
 				if err != nil {
 					return fmt.Errorf("error loading passthru-secret file %s", path)
 				}
-				fmt.Printf(info.Name())
+
 				passthruSecrets[info.Name()] = strings.TrimSpace(string(data))
 				return nil
 			})
