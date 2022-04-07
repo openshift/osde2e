4d12fbed55009762d913bcbeb90bc9d990024b5c
Diego Santamaria <dsantama@redhat.com>
2022-04-07 15:38:24 -0600

    Added logging for aws creds

--- pkg/e2e/operators/cloudingress/rhapi_lb.go
+++ pkg/e2e/operators/cloudingress/rhapi_lb.go
@@ -90,6 +90,15 @@ func testLBDeletion(h *helper.H) {
 
 		if viper.GetString(config.CloudProvider.CloudProviderID) == "aws" {
 			util.GinkgoIt("manually deleted LB should be recreated in AWS", func() {
+				if viper.GetString("ocm.aws.accessKey") == "" {
+					log.Printf("aws access are not being passed in this scope")
+				}
+				if viper.GetString("ocm.aws.secretKey") == "" {
+					log.Printf("aws secret are not being passed in this scope")
+				}
+				if viper.GetString(config.CloudProvider.Region) == "" {
+					log.Printf("aws secret are not being passed in this scope")
+				}
 				awsAccessKey := viper.GetString("ocm.aws.accessKey")
 				awsSecretKey := viper.GetString("ocm.aws.secretKey")
 				awsRegion := viper.GetString(config.CloudProvider.Region)
