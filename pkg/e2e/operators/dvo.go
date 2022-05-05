d33683e351ba3db2ff868d984980ab4765a29ca6
npecka <zangoose107@gmail.com>
2022-05-05 12:20:45 -0400

    Adding DVO to osde2e
    Signed-off-by: npecka <zangoose107@gmail.com>

--- pkg/e2e/operators/dvo.go
+++ pkg/e2e/operators/dvo.go
@@ -1,16 +1,16 @@
 package operators
 
 import (
-	// "reflect" this is needed when PR https://github.com/openshift/route-monitor-operator/pull/94 is merged
-
 	"context"
 	"fmt"
+	"log"
 	"regexp"
 	"strings"
 	"time"
 
 	"github.com/onsi/ginkgo"
-	//. "github.com/onsi/gomega" // go-staticcheck ST1001  should not use dot imports
+	operatorv1 "github.com/operator-framework/api/pkg/operators/v1"
+	operatorv1alpha "github.com/operator-framework/api/pkg/operators/v1alpha1"
 
 	"github.com/openshift/osde2e/pkg/common/alert"
 	"github.com/openshift/osde2e/pkg/common/config"
@@ -21,6 +21,7 @@ import (
 	viper "github.com/openshift/osde2e/pkg/common/concurrentviper"
 	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 )
@@ -31,9 +32,9 @@ func init() {
 	alert.RegisterGinkgoAlert(deploymentValidationOperatorTestName, "SD-SREP", "Ron Green", "sd-cicd-alerts", "sd-cicd@redhat.com", 4)
 }
 
-var _ = ginkgo.Describe(deploymentValidationOperatorTestName, func() {
+var _ = ginkgo.FDescribe(deploymentValidationOperatorTestName, func() {
 	const (
-		operatorNamespace         = "deployment-validation-operator"
+		operatorNamespace         = "osde2e-deployment-validation-operator"
 		operatorName              = "deployment-validation-operator"
 		operatorDeploymentName    = "deployment-validation-operator"
 		operatorCsvDisplayName    = "Deployment Validation Operator"
@@ -49,17 +50,33 @@ var _ = ginkgo.Describe(deploymentValidationOperatorTestName, func() {
 
 		operatorLockFile = "deployment-validation-operator-lock"
 
-		defaultDesiredReplicas int32 = 3
+		defaultDesiredReplicas int32 = 1
 	)
 
 	var clusterRoles = []string{
-		"deployment-validation-operator",
+		"deployment-validation-operator-admin",
+		"deployment-validation-operator-edit",
+		"deployment-validation-operator-view",
 	}
 
 	h := helper.New()
 	nodeLabels := make(map[string]string)
 
-	//checkClusterServiceVersion(h, operatorNamespace, operatorCsvDisplayName)
+	//Create DVO project and install operator
+	deployDVO(helper.New(),
+		operatorNamespace,
+		operatorName,
+		operatorName,
+		"deployment-validation-operator")
+
+	// Future test once new versions are in place for DVO
+	//checkUpgrade(helper.New(),
+	//	operatorNamespace,
+	//	operatorName,
+	//	operatorName,
+	//	"deployment-validation-operator-registry")
+
+	checkClusterServiceVersion(h, operatorNamespace, operatorCsvDisplayName)
 	checkConfigMapLockfile(h, operatorNamespace, operatorLockFile)
 	checkDeployment(h, operatorNamespace, operatorDeploymentName, defaultDesiredReplicas)
 	checkClusterRoles(h, clusterRoles, false)
@@ -108,11 +125,12 @@ var _ = ginkgo.Describe(deploymentValidationOperatorTestName, func() {
 
 	}, float64(viper.GetFloat64(config.Tests.PollingTimeout)))
 
-	//checkUpgrade(helper.New(),
-	//	operatorNamespace,
-	//	operatorName,
-	//	operatorName,
-	//	"deployment-validation-operator")
+	// Teardown DVO (Project, Operator Group, and Subscription)
+	deleteDVO(helper.New(),
+		operatorNamespace,
+		operatorName,
+		operatorName,
+		"deployment-validation-operator")
 })
 
 // Function to create a standard deployment
@@ -159,4 +177,103 @@ func regexDVOCheck(filterValue string, data string, deploymentName string) bool 
 		}
 	}
 	return false
+}
+
+// Create DVO Subscription
+func deployDVO(h *helper.H, subNamespace string, subName string, packageName string, regServiceName string) {
+
+	ginkgo.Context("Install DVO", func() {
+		ginkgo.It("should install DVO for future tests", func() {
+
+			//Setup vars for error and target namespace for Operator Group
+			var err error
+			var targetns = []string{
+				"osde2e-deployment-validation-operator",
+			}
+
+			// Create DVO Namespace
+			h.CreateProject(subName)
+
+			// Create Operator Group
+			_, err = h.Operator().OperatorsV1().OperatorGroups(subNamespace).Create(context.TODO(), &operatorv1.OperatorGroup{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      subName,
+					Namespace: subNamespace,
+				},
+				Spec: operatorv1.OperatorGroupSpec{
+					TargetNamespaces: targetns,
+				},
+			}, metav1.CreateOptions{})
+			Expect(err).NotTo(HaveOccurred(), fmt.Sprintf("failed trying to create Operator Group %s", subName))
+
+			// Create Subscription
+			_, err = h.Operator().OperatorsV1alpha1().Subscriptions(subNamespace).Create(context.TODO(), &operatorv1alpha.Subscription{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      subName,
+					Namespace: subNamespace,
+				},
+				Spec: &operatorv1alpha.SubscriptionSpec{
+					Package:                "deployment-validation-operator",
+					Channel:                "alpha",
+					CatalogSourceNamespace: "openshift-marketplace",
+					CatalogSource:          "community-operators",
+					InstallPlanApproval:    operatorv1alpha.ApprovalAutomatic,
+				},
+			}, metav1.CreateOptions{})
+			Expect(err).NotTo(HaveOccurred(), fmt.Sprintf("failed trying to create Subscription %s", subName))
+
+			log.Printf("Created DVO subscription %s", subName)
+
+		}, float64(viper.GetFloat64(config.Tests.PollingTimeout)))
+	})
+}
+
+// Delete DVO Subscription
+func deleteDVO(h *helper.H, subNamespace string, subName string, packageName string, regServiceName string) {
+
+	ginkgo.Context("Operator Upgrade", func() {
+		ginkgo.It("should upgrade from the replaced version", func() {
+
+			// Get the CSV we're currently installed with
+			var latestCSV string
+			var sub *operatorv1alpha.Subscription
+			var err error
+
+			pollErr := wait.PollImmediate(5*time.Second, 5*time.Minute, func() (bool, error) {
+				sub, err = h.Operator().OperatorsV1alpha1().Subscriptions(subNamespace).Get(context.TODO(), subName, metav1.GetOptions{})
+				if err != nil {
+					return false, err
+				}
+				latestCSV = sub.Status.CurrentCSV
+				if latestCSV != "" {
+					return true, nil
+				}
+				return false, nil
+			})
+			Expect(pollErr).NotTo(HaveOccurred(), fmt.Sprintf("failed trying to get Subscription %s in %s namespace: %v", subName, subNamespace, err))
+
+			// Delete current Operator installation
+			err = h.Operator().OperatorsV1().OperatorGroups(subNamespace).Delete(context.TODO(), subName, metav1.DeleteOptions{})
+			Expect(err).NotTo(HaveOccurred(), fmt.Sprintf("failed trying to delete operator group %s", subName))
+			log.Printf("Removed operator group %s", subName)
+
+			err = h.Operator().OperatorsV1alpha1().Subscriptions(subNamespace).Delete(context.TODO(), subName, metav1.DeleteOptions{})
+			Expect(err).NotTo(HaveOccurred(), fmt.Sprintf("failed trying to delete Subscription %s", subName))
+			log.Printf("Removed subscription %s", subName)
+
+			err = h.Operator().OperatorsV1alpha1().ClusterServiceVersions(subNamespace).Delete(context.TODO(), latestCSV, metav1.DeleteOptions{})
+			Expect(err).NotTo(HaveOccurred(), fmt.Sprintf("failed trying to delete ClusterServiceVersion %s", latestCSV))
+			log.Printf("Removed csv %s", latestCSV)
+
+			err = helper.DeleteNamespace(subNamespace, true, h)
+			Expect(err).NotTo(HaveOccurred(), fmt.Sprintf("failed trying to delete project %s", subNamespace))
+			log.Printf("Removed project %s", subNamespace)
+
+			Eventually(func() bool {
+				_, err := h.Operator().OperatorsV1alpha1().InstallPlans(subNamespace).Get(context.TODO(), sub.Status.Install.Name, metav1.GetOptions{})
+				return apierrors.IsNotFound(err)
+			}, 5*time.Minute, 10*time.Second).Should(BeTrue(), "installplan never garbage collected")
+			log.Printf("Verified installplan removal")
+		}, float64(viper.GetFloat64(config.Tests.PollingTimeout)))
+	})
 }
