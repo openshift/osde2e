fb02cb1cda6d72118ce5fd20fd3e183009abc144
npecka <zangoose107@gmail.com>
2022-05-05 13:35:34 -0400

    Removed F in the ginko test
    Signed-off-by: npecka <zangoose107@gmail.com>

--- pkg/e2e/operators/dvo.go
+++ pkg/e2e/operators/dvo.go
@@ -32,7 +32,7 @@ func init() {
 	alert.RegisterGinkgoAlert(deploymentValidationOperatorTestName, "SD-SREP", "Ron Green", "sd-cicd-alerts", "sd-cicd@redhat.com", 4)
 }
 
-var _ = ginkgo.FDescribe(deploymentValidationOperatorTestName, func() {
+var _ = ginkgo.Describe(deploymentValidationOperatorTestName, func() {
 	const (
 		operatorNamespace         = "osde2e-deployment-validation-operator"
 		operatorName              = "deployment-validation-operator"
