fe5c8fd6bff45712ebcb440b7f14a6b9ad6f4e8b
Brady Pratt <bpratt@redhat.com>
2023-01-20 04:21:22 -0600

    e2e/verify: refactor and simplify loadbalancers test
    Signed-off-by: Brady Pratt <bpratt@redhat.com>

--- pkg/e2e/verify/loadbalancers.go
+++ pkg/e2e/verify/loadbalancers.go
@@ -2,17 +2,18 @@ package verify
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	v1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	"github.com/openshift/osde2e/pkg/common/alert"
+	viper "github.com/openshift/osde2e/pkg/common/concurrentviper"
+	"github.com/openshift/osde2e/pkg/common/config"
+	"github.com/openshift/osde2e/pkg/common/expect"
 	"github.com/openshift/osde2e/pkg/common/helper"
 	"github.com/openshift/osde2e/pkg/common/label"
-	"github.com/openshift/osde2e/pkg/common/util"
+	"github.com/openshift/osde2e/pkg/common/providers/rosaprovider"
+	v1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/e2e-framework/klient/k8s/resources"
 )
 
 const (
@@ -28,31 +29,25 @@ func init() {
 	alert.RegisterGinkgoAlert(loadBalancersTestName, "SD-CICD", "Diego Santamaria", "sd-cicd-alerts", "sd-cicd@redhat.com", 4)
 }
 
-var _ = ginkgo.Describe(loadBalancersTestName, label.Informing, func() {
-	h := helper.New()
+var _ = ginkgo.Describe(loadBalancersTestName, ginkgo.Ordered, label.Informing, func() {
+	var h *helper.H
+	var client *resources.Resources
+	ginkgo.BeforeAll(func() {
+		h = helper.New()
+		client = h.AsUser("")
+	})
 
-	util.GinkgoIt("router/ingress load balancer should exist", func(ctx context.Context) {
-		exists, err := loadBalancerExists(ctx, h, routerIngressLoadBalancerNamespace, routerIngressLoadBalancer)
-		Expect(err).ToNot(HaveOccurred(), "an error should not have occurred when looking for the load balancer")
-		Expect(exists).To(BeTrue(), "the load balancer should exist")
-	}, 10)
+	ginkgo.DescribeTable("should exist", func(ctx context.Context, name, namespace string) {
+		if name == externalLoadBalancer && (viper.GetBool(config.Hypershift) || viper.GetBool(rosaprovider.STS)) {
+			ginkgo.Skip("rh-api load balancer is not deployed to ROSA or HyperShift clusters")
+		}
 
-	util.GinkgoIt("external load balancer should exist", func(ctx context.Context) {
-		exists, err := loadBalancerExists(ctx, h, externalLoadBalancerNamespace, externalLoadBalancer)
-		Expect(err).ToNot(HaveOccurred(), "an error should not have occurred when looking for the load balancer")
-		Expect(exists).To(BeTrue(), "the load balancer should exist")
-	}, 10)
+		service := &v1.Service{}
+		expect.NoError(client.Get(ctx, name, namespace, service))
+		Expect(service.Spec.Type).To(Equal(v1.ServiceTypeLoadBalancer), "expected a load balancer service but got %s", service.Spec.Type)
+
+	},
+		ginkgo.Entry("router-default", routerIngressLoadBalancer, routerIngressLoadBalancerNamespace),
+		ginkgo.Entry("rh-api", externalLoadBalancer, externalLoadBalancerNamespace),
+	)
 })
-
-func loadBalancerExists(ctx context.Context, h *helper.H, namespace string, loadBalancer string) (bool, error) {
-	service, err := h.Kube().CoreV1().Services(namespace).Get(ctx, loadBalancer, metav1.GetOptions{})
-	if err != nil {
-		return false, fmt.Errorf("error getting loadbalancer: %v", err)
-	}
-
-	if service.Spec.Type != v1.ServiceTypeLoadBalancer {
-		return false, fmt.Errorf("namespace %s, service %s is not a load balancer, but is type %v", namespace, loadBalancer, service.Spec.Type)
-	}
-
-	return true, nil
-}
