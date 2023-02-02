3668b834597d4da2e0551358d5d43c7b6de97044
Brady Pratt <bpratt@redhat.com>
2023-02-02 08:52:24 -0600

    e2e/verify/prom_exporters: drop util.GinkgoIt
    Signed-off-by: Brady Pratt <bpratt@redhat.com>

--- pkg/e2e/verify/prom_exporters.go
+++ pkg/e2e/verify/prom_exporters.go
@@ -88,7 +88,7 @@ var _ = ginkgo.Describe(promExportersTestname, label.E2E, func() {
 
 	h := helper.New()
 
-	util.GinkgoIt("should exist and be running in the cluster", func(ctx context.Context) {
+	ginkgo.It("should exist and be running in the cluster", func(ctx context.Context) {
 		envs := []string{allProviders, viper.GetString(config.CloudProvider.CloudProviderID)}
 
 		// Expect project to exist
@@ -109,7 +109,7 @@ var _ = ginkgo.Describe(promExportersTestname, label.E2E, func() {
 
 		// Ensure services are present
 		checkServices(ctx, promNamespace, servicesToCheck, h, envs...)
-	}, 300)
+	})
 })
 
 func checkConfigMaps(ctx context.Context, namespace string, configMaps map[string][]string, h *helper.H, providers ...string) {
