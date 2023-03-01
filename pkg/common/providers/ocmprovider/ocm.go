e22df8ef81fd4a5a930397ed5402973e86f0a491
Brian Smith <bsmit@redhat.com>
2023-03-01 10:25:16 -0500

    adding FR special ClientIDs

--- pkg/common/providers/ocmprovider/ocm.go
+++ pkg/common/providers/ocmprovider/ocm.go
@@ -17,20 +17,24 @@ const (
 	APIVersion = "v1"
 
 	// commercialTokenURL specifies the endpoint used to create access tokens in the commercial environment.
-	commercialTokenURL     = "https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token"
+	commercialTokenURL = "https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token"
 	// The following TokenURLs are specific to the FedRAMP environment
 	govintegrationTokenURL = "https://rh-ocm-appsre-integration.auth-fips.us-gov-west-1.amazoncognito.com/oauth2/token"
 	govstageTokenURL       = "https://ocm-ra-stage-domain.auth-fips.us-gov-west-1.amazoncognito.com/oauth2/token"
 	govproductionTokenURL  = "https://ocm-ra-production-domain.auth-fips.us-gov-west-1.amazoncognito.com/oauth2/token"
 
 	// ClientID is used to identify the client to OSD.
-	ClientID = "cloud-services"
+	commercialClientID = "cloud-services"
+	// The following ClientIDs are specific to the FedRAMP environment
+	govintegrationClientID = "20fbrpgl28f8oehp6709mk3nnr"
+	govstageClientID       = "1lb687dlpsmsfuj53r3je06vpp"
+	govproductionClientID  = "72ekjh5laouap6qcfis521jlgi"
 )
 
 type ocmConnectionKey struct {
-	token    string
-	env      string
-	debug    bool
+	token string
+	env   string
+	debug bool
 }
 
 var connectionCache = map[ocmConnectionKey]*ocm.Connection{}
@@ -52,6 +56,7 @@ func init() {
 // OCMConnection returns a raw OCM connection.
 func OCMConnection(token, env string, debug bool) (*ocm.Connection, error) {
 	TokenURL := ""
+	ClientID := ""
 
 	cacheKey := ocmConnectionKey{
 		token: token,
@@ -79,12 +84,16 @@ func OCMConnection(token, env string, debug bool) (*ocm.Connection, error) {
 	switch {
 	case strings.Contains(url, "api.int.openshiftusgov.com"):
 		TokenURL = govintegrationTokenURL
+		ClientID = govintegrationClientID
 	case strings.Contains(url, "api.stage.openshiftusgov.com"):
 		TokenURL = govstageTokenURL
+		ClientID = govstageClientID
 	case strings.Contains(url, "api.openshiftusgov.com"):
 		TokenURL = govproductionTokenURL
+		ClientID = govproductionClientID
 	default:
 		TokenURL = commercialTokenURL
+		ClientID = commercialClientID
 	}
 
 	builder := ocm.NewConnectionBuilder().
