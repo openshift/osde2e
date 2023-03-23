9104cb330efd1edcd8d656999cf177a59d82debd
Ryan Williams <3375653+ryankwilliams@users.noreply.github.com>
2023-03-23 14:49:32 -0400

    New version selector to get latest x.y version available
    This change adds a new install selector to select a version to install
    for rosa/osd based on a major.minor version supplied. It will get the
    latest available one based on the channel group provided as well.

---
+++ pkg/common/versions/installselectors/latest_xy.go
@@ -0,0 +1,43 @@
+package installselectors
+
+import (
+	"fmt"
+
+	"github.com/Masterminds/semver"
+	viper "github.com/openshift/osde2e/pkg/common/concurrentviper"
+	"github.com/openshift/osde2e/pkg/common/config"
+	"github.com/openshift/osde2e/pkg/common/spi"
+)
+
+func init() {
+	registerSelector(latestXYVersion{})
+}
+
+type latestXYVersion struct{}
+
+func (m latestXYVersion) ShouldUse() bool {
+	return viper.GetString(config.Cluster.InstallLatestXY) != ""
+}
+
+func (m latestXYVersion) Priority() int {
+	return 60
+}
+
+func (m latestXYVersion) SelectVersion(versionList *spi.VersionList) (*semver.Version, string, error) {
+	latestXY := viper.GetString(config.Cluster.InstallLatestXY)
+	versionType := "latest X.Y version available"
+	versions := versionList.AvailableVersions()
+
+	semVersion, err := semver.NewVersion(latestXY)
+	if err != nil {
+		return nil, versionType, fmt.Errorf("error parsing semantic version for %s", latestXY)
+	}
+
+	for _, version := range versions {
+		if version.Version().Major() == semVersion.Major() && version.Version().Minor() == semVersion.Minor() {
+			return version.Version(), versionType, nil
+		}
+	}
+
+	return nil, versionType, fmt.Errorf("unable to locate latest version for %s", latestXY)
+}
