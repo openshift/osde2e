eb9267644b9df8891ccff81af3f79e601ab573bf
Ritu Mundhe <rmundhe@redhat.com>
2023-04-20 16:55:31 -0500

    adding missing required variables for rosa job script (#1781)
    * adding more required variables for rosa jobs
    
    * removed flag invalid for docker

--- scripts/parameterized-job.sh
+++ scripts/parameterized-job.sh
@@ -2,13 +2,14 @@
 
 set +e
 # ensure we have a clean environment
-docker rm -i osde2e-run
+docker rm osde2e-run
 
 # bind mounts run into permissions issues, this creates
 # the container and copies the secrets over to ensure it has perms
 docker create --name osde2e-run -e OCM_TOKEN \
 	-e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY \
-	-e CLOUD_PROVIDER_REGION -e INSTANCE_TYPE \
+	-e CLOUD_PROVIDER_REGION -e ROSA_AWS_REGION="${CLOUD_PROVIDER_REGION}" \
+	-e ROSA_ENV=stage -e INSTANCE_TYPE \
 	-e REPORT_DIR=/tmp/osde2e-report quay.io/app-sre/osde2e test --configs rosa,stage,e2e-suite --skip-health-check
 
 docker start -a osde2e-run
