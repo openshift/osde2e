3c17e06c9745eb3dd06f4786739385f8261f3c86
Ritu Mundhe <rmundhe@redhat.com>
2023-04-21 13:59:00 -0500

    added params to jenkins job script

--- scripts/parameterized-job.sh
+++ scripts/parameterized-job.sh
@@ -8,9 +8,12 @@ docker rm osde2e-run
 # the container and copies the secrets over to ensure it has perms
 docker create --name osde2e-run -e OCM_TOKEN \
 	-e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY \
-	-e CLOUD_PROVIDER_REGION -e ROSA_AWS_REGION="${CLOUD_PROVIDER_REGION}" \
-	-e ROSA_ENV=stage -e ROSA_STS=true -e INSTANCE_TYPE \
-	-e REPORT_DIR="${REPORT_DIR}" quay.io/app-sre/osde2e test --configs rosa,stage,e2e-suite 
+	-e CLOUD_PROVIDER_REGION \
+	-e ROSA_AWS_REGION="${CLOUD_PROVIDER_REGION}" \
+	-e ROSA_ENV=stage \
+	-e ROSA_STS="${STS}" \
+	-e INSTANCE_TYPE \
+	-e REPORT_DIR="${REPORT_DIR}" quay.io/app-sre/osde2e test --configs "${CONFIGS}"
 
 docker start -a osde2e-run
 
