63f903f240a9677d804f13c7a952ebf5ff66f1d7
Ritu Mundhe <rmundhe@redhat.com>
2023-05-23 15:22:23 -0500

    gofumpt

--- pkg/common/helper/testRunner.go
+++ pkg/common/helper/testRunner.go
@@ -94,7 +94,6 @@ func (h *H) RunTests(ctx context.Context, name string, timeout int, harnesses, a
 
 		// get results
 		results, err := r.RetrieveTestResults()
-
 		if err != nil {
 			log.Print("error while reading test results: %v", err)
 		}
