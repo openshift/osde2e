425048da758b1ec2481bdc3bc1025bddb1d50996
Ritu Mundhe <rmundhe@redhat.com>
2023-07-13 11:05:16 -0500

    PR review updates

--- pkg/common/helper/runner.go
+++ pkg/common/helper/runner.go
@@ -6,10 +6,23 @@ import (
 
 	. "github.com/onsi/gomega"
 	viper "github.com/openshift/osde2e/pkg/common/concurrentviper"
+	"github.com/openshift/osde2e/pkg/common/templates"
 
 	"github.com/openshift/osde2e/pkg/common/config"
 	"github.com/openshift/osde2e/pkg/common/runner"
 )
+
+// RunnerWithTemplateCommand creates an extended test suite runner for templated test harness pods, configures RBAC for it.
+func (h *H) RunnerWithTemplateCommand(timeout float64, harness string, suffix string, jobName string, serviceAccountDir string) *runner.Runner {
+	r := h.RunnerWithNoCommand()
+	r.PodSpec.ServiceAccountName = h.GetNamespacedServiceAccount()
+	latestImageStream, err := r.GetLatestImageStreamTag()
+	Expect(err).NotTo(HaveOccurred(), "Could not get latest imagestream tag")
+	cmd, err := getCommandString(h, timeout, latestImageStream, harness, suffix, jobName, serviceAccountDir)
+	Expect(err).NotTo(HaveOccurred(), "Could not create pod command from template")
+	r.Cmd = cmd
+	return r
+}
 
 // RunnerWithNoCommand creates an extended test suite runner and configure RBAC for it.
 func (h *H) RunnerWithNoCommand() *runner.Runner {
@@ -42,4 +55,50 @@ func (h *H) WriteResults(results map[string][]byte) {
 		err = os.WriteFile(dst, data, os.ModePerm)
 		Expect(err).NotTo(HaveOccurred())
 	}
+}
+
+// Generates templated command string to provide to test harness container
+func getCommandString(h *H, timeout float64, latestImageStream string, harness string, suffix string, jobName string, serviceAccountDir string) (string, error) {
+	values := struct {
+		Name                 string
+		JobName              string
+		Arguments            string
+		Timeout              int
+		Image                string
+		OutputDir            string
+		ServiceAccount       string
+		PushResultsContainer string
+		Suffix               string
+		Server               string
+		CA                   string
+		TokenFile            string
+		EnvironmentVariables []struct {
+			Name  string
+			Value string
+		}
+	}{
+		Name:                 jobName,
+		JobName:              jobName,
+		Timeout:              int(timeout),
+		Image:                harness,
+		OutputDir:            runner.DefaultRunner.OutputDir,
+		ServiceAccount:       h.GetNamespacedServiceAccount(),
+		PushResultsContainer: latestImageStream,
+		Suffix:               suffix,
+		Server:               "https://kubernetes.default",
+		CA:                   serviceAccountDir + "/ca.crt",
+		TokenFile:            serviceAccountDir + "/token",
+		EnvironmentVariables: []struct {
+			Name  string
+			Value string
+		}{
+			{
+				Name:  "OCM_CLUSTER_ID",
+				Value: viper.GetString(config.Cluster.ID),
+			},
+		},
+	}
+	testTemplate, err := templates.LoadTemplate("tests/tests-runner.template")
+	Expect(err).NotTo(HaveOccurred(), "Could not load pod template")
+	return h.ConvertTemplateToString(testTemplate, values)
 }
--- pkg/e2e/harness_runner/harness_runner.go
+++ pkg/e2e/harness_runner/harness_runner.go
@@ -13,108 +13,65 @@ import (
 	"github.com/openshift/osde2e/pkg/common/helper"
 	"github.com/openshift/osde2e/pkg/common/label"
 	"github.com/openshift/osde2e/pkg/common/runner"
-	"github.com/openshift/osde2e/pkg/common/templates"
 	"github.com/openshift/osde2e/pkg/common/util"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var (
 	serviceAccountDir = "/var/run/secrets/kubernetes.io/serviceaccount"
+	serviceAccount    = "system:serviceaccount:%s:cluster-admin"
 	TimeoutInSeconds  = viper.GetFloat64(config.Tests.PollingTimeout)
 	harnesses         = strings.Split(viper.GetString(config.Tests.TestHarnesses), ",")
 	h                 *helper.H
 	HarnessEntries    []ginkgo.TableEntry
+	harness           string
+	r                 *runner.Runner
+	jobName           string
+	suffix            string
 )
 
-var _ = ginkgo.Describe("Test Harness", ginkgo.Ordered, label.TestHarness, func() {
+var _ = ginkgo.Describe("Test Harness", ginkgo.Ordered, ginkgo.ContinueOnFailure, label.TestHarness, func() {
 	for _, harness := range harnesses {
 		HarnessEntries = append(HarnessEntries, ginkgo.Entry("should run "+harness+" successfully", harness))
 	}
+
+	ginkgo.BeforeEach(func(ctx context.Context) {
+		// Run harness in a new project
+		viper.Set(config.Project, "")
+		h = helper.New()
+		h.SetServiceAccount(ctx, serviceAccount)
+		suffix = util.RandomStr(5)
+	})
+
+	ginkgo.AfterEach(func(ctx context.Context) {
+		// get results
+		results, err := r.RetrieveTestResults()
+		Expect(err).NotTo(HaveOccurred(), "Could not read results")
+
+		// write results
+		h.WriteResults(results)
+		h.Cleanup(ctx)
+	})
+
 	ginkgo.DescribeTable("Executing Harness",
-		func(harness string) {
+		func(ctx context.Context, harness string) {
 			ginkgo.By("======= RUNNING HARNESS: " + harness + " =======")
 			log.Printf("======= RUNNING HARNESS: %s =======", harness)
-			viper.Set(config.Project, "")
-			// Run harness in new project
-			h = helper.New()
-			h.SetServiceAccount(context.TODO(), "system:serviceaccount:%s:cluster-admin")
 			harnessImageIndex := strings.LastIndex(harness, "/")
 			harnessImage := harness[harnessImageIndex+1:]
-			suffix := util.RandomStr(5)
 			jobName := fmt.Sprintf("%s-%s", harnessImage, suffix)
-			r := h.RunnerWithNoCommand()
-			testCommand, err := getCommandString(h, harness, r, suffix, jobName)
-			Expect(err).NotTo(HaveOccurred(), "Could not create test pod template")
-			r.Cmd = testCommand
+			r = h.RunnerWithTemplateCommand(TimeoutInSeconds, harness, suffix, jobName, serviceAccountDir)
 
 			// run tests
 			stopCh := make(chan struct{})
-			err = r.Run(int(TimeoutInSeconds), stopCh)
+			err := r.Run(int(TimeoutInSeconds), stopCh)
 			Expect(err).NotTo(HaveOccurred(), "Could not run pod")
 
-			// get results
-			results, err := r.RetrieveTestResults()
-			Expect(err).NotTo(HaveOccurred(), "Could not read results")
-
-			// write results
-			h.WriteResults(results)
-
 			// ensure job has not failed
-			_, err = h.Kube().BatchV1().Jobs(r.Namespace).Get(context.TODO(), jobName, metav1.GetOptions{})
+			_, err = h.Kube().BatchV1().Jobs(r.Namespace).Get(ctx, jobName, metav1.GetOptions{})
 			Expect(err).NotTo(HaveOccurred(), "Harness job pods failed")
 
-			h.Cleanup(context.TODO())
 			ginkgo.By("======= FINISHED HARNESS: " + harness + " =======")
 		},
 		HarnessEntries)
 })
-
-// Generates templated command string to provide to test harness container
-func getCommandString(h *helper.H, harness string, r *runner.Runner, suffix string, jobName string) (string, error) {
-	// setup runner
-	latestImageStream, err := r.GetLatestImageStreamTag()
-	Expect(err).NotTo(HaveOccurred(), "Could not get latest imagestream tag")
-
-	values := struct {
-		Name                 string
-		JobName              string
-		Arguments            string
-		Timeout              int
-		Image                string
-		OutputDir            string
-		ServiceAccount       string
-		PushResultsContainer string
-		Suffix               string
-		Server               string
-		CA                   string
-		TokenFile            string
-		EnvironmentVariables []struct {
-			Name  string
-			Value string
-		}
-	}{
-		Name:                 jobName,
-		JobName:              jobName,
-		Timeout:              int(TimeoutInSeconds),
-		Image:                harness,
-		OutputDir:            runner.DefaultRunner.OutputDir,
-		ServiceAccount:       h.GetNamespacedServiceAccount(),
-		PushResultsContainer: latestImageStream,
-		Suffix:               suffix,
-		Server:               "https://kubernetes.default",
-		CA:                   serviceAccountDir + "/ca.crt",
-		TokenFile:            serviceAccountDir + "/token",
-		EnvironmentVariables: []struct {
-			Name  string
-			Value string
-		}{
-			{
-				Name:  "OCM_CLUSTER_ID",
-				Value: viper.GetString(config.Cluster.ID),
-			},
-		},
-	}
-	testTemplate, err := templates.LoadTemplate("tests/tests-runner.template")
-	Expect(err).NotTo(HaveOccurred(), "Could not load pod template")
-	return h.ConvertTemplateToString(testTemplate, values)
-}
