openshift/osde2e

commit fb6fe3d88f255f983f354ff1415a9632077e2e09

pr-check updates

pkg/debug/diff.go
@@ -133,11 +133,6 @@ func GetCurrentMCCHash() (hash string, err error) {
 func getLastJobID(baseProwURL, jobName string) (int, error) {
 	// Look up the list of previous jobs with a given name
 	var url string
-	if viper.GetBool(config.Tests.EnablePrCheck) {
-		url = fmt.Sprintf("%s/job-history/gs/origin-ci-test/pr-logs/directory/%s", baseProwURL, jobName)
-	} else {
-		url = fmt.Sprintf("%s/job-history/gs/origin-ci-test/logs/%s", baseProwURL, jobName)
-	}
 	log.Printf("Looking up job history from %s", url)
 	res, err := http.Get(url)
 	if err != nil {

ci/prow_pr_check.sh
@@ -16,10 +16,9 @@ set -eo pipefail
 
     make build
 
-    CLUSTER_ID=1s5hroubg5u8ucn7ijkduo0derrnq7hv \
+    CLUSTER_ID=1s82os6n9jnmmrc2dnec673o1lgavgs1 \
     GINKGO_SKIP="Must Gather Operator" \
     OCM_CCS="true" \
-    HIBERNATE_AFTER_USE="false" \
-    ./out/osde2e test --configs=prod,aws,pr-check,e2e-suite --secret-locations=/usr/local/osde2e-common,/usr/local/osde2e-credentials,/usr/local/sd-cicd-aws-prod
+    ./out/osde2e test --configs=prod,aws,e2e-suite --secret-locations=/usr/local/osde2e-common,/usr/local/osde2e-credentials,/usr/local/sd-cicd-aws-prod
 
 } 2>&1 | tee -a "$REPORT_DIR/test_output.log"

cmd/osde2e/test/cmd.go
@@ -132,7 +132,6 @@ func init() {
 	viper.BindPFlag(config.Tests.GinkgoFocus, Cmd.PersistentFlags().Lookup("focus-tests"))
 	viper.BindPFlag(config.Tests.GinkgoSkip, Cmd.PersistentFlags().Lookup("skip-tests"))
 	viper.BindPFlag(config.MustGather, Cmd.PersistentFlags().Lookup("must-gather"))
-	viper.BindPFlag(config.Tests.EnablePrCheck, Cmd.PersistentFlags().Lookup("enable-pr-check"))
 }
 
 func run(cmd *cobra.Command, argv []string) {

configs/pr-check.yaml
@@ -1,2 +1,0 @@
-tests:
-  EnablePrCheck: true

pkg/common/cluster/pr_check_queue.go
@@ -1,88 +1,0 @@
-package cluster
-
-import (
-	"context"
-	"fmt"
-	"strings"
-	"time"
-
-	"github.com/openshift/osde2e/pkg/common/clusterproperties"
-	viper "github.com/openshift/osde2e/pkg/common/concurrentviper"
-	"github.com/openshift/osde2e/pkg/common/config"
-	"github.com/openshift/osde2e/pkg/common/helper"
-	"github.com/openshift/osde2e/pkg/common/spi"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/wait"
-)
-
-var (
-	interval = 5 * time.Minute
-	timeout  = 190 * time.Minute
-)
-
-func PrCheckQueue(o spi.Provider) error {
-
-	cluster, err := o.GetCluster(viper.GetString(config.Cluster.ID))
-	if err != nil {
-		return fmt.Errorf("error getting cluster: %v", err)
-	}
-
-	properties := cluster.Properties()
-
-	err = wait.PollImmediate(interval, timeout, func() (bool, error) {
-
-		if properties[clusterproperties.JobID] == "" {
-			err = o.AddProperty(cluster, clusterproperties.JobStartedAt, viper.GetString(config.JobStartedAt))
-			if err != nil {
-				return false, fmt.Errorf("error adding property to cluster: %v", err)
-			}
-			return true, nil
-		}
-
-		startTime, err := time.Parse(time.RFC3339, viper.GetString(config.JobStartedAt))
-		if err != nil {
-			return false, fmt.Errorf("error parsing Time for queue started at: %v", err)
-		}
-
-		//Check that there is a running job and if there is, check that it is not older than 3 hours
-		if properties[clusterproperties.JobID] != "" && startTime.After(startTime.Add(3*time.Hour)) {
-			if err := prCheckClean(o, cluster); err != nil {
-				return false, fmt.Errorf("error cleaning up aborted jobs: %v", err)
-			}
-		}
-
-		return false, err
-	})
-	return nil
-}
-
-func prCheckClean(o spi.Provider, cluster *spi.Cluster) error {
-
-	h := helper.NewOutsideGinkgo()
-
-	namespaces, err := h.Kube().CoreV1().Namespaces().List(context.Background(), metav1.ListOptions{})
-	if err != nil {
-		return fmt.Errorf("error getting namespaces: %v", err)
-	}
-
-	//This is a very slow implementation. Can KubeClient be used to list namespaces on a regex or can we add a filter to the list?
-	for _, namespace := range namespaces.Items {
-		if strings.Contains(namespace.Name, "osde2e-") {
-			if err := h.Kube().CoreV1().Namespaces().Delete(context.Background(), namespace.Name, metav1.DeleteOptions{}); err != nil {
-				return fmt.Errorf("error deleting namespace: %v", err)
-			}
-		}
-	}
-
-	//Sets the JobID to empty string in order to exit the polling loop
-	if err = o.AddProperty(cluster, clusterproperties.JobID, ""); err != nil {
-		return fmt.Errorf("error adding property to cluster: %v", err)
-	}
-
-	//Sets the job started at to the current job's start time.
-	if err = o.AddProperty(cluster, clusterproperties.JobStartedAt, viper.GetString(config.JobStartedAt)); err != nil {
-		return fmt.Errorf("error adding property to cluster: %v", err)
-	}
-
-	return nil
-}

pkg/common/clusterproperties/properties.go
@@ -25,7 +25,4 @@ const (
 
 	// ProvisionShardID is the shard ID that is set to provision a shard for the cluster.
 	ProvisionShardID = "provision_shard_id"
-
-	// Osde2eStartTime is the time when Osde2e started to run.
-	JobStartedAt = "JobStartedAt"
 )

pkg/common/config/config.go
@@ -231,10 +231,6 @@ var Tests = struct {
 	// EnableFips enables the FIPS test suite
 	// Env: ENABLE_FIPS
 	EnableFips string
-
-	// EnablePrCheck enables the PR check Mode
-	// Env: ENABLE_PR_CHECK
-	EnablePrCheck string
 }{
 
 	PollingTimeout:             "tests.pollingTimeout",
@@ -249,7 +245,6 @@ var Tests = struct {
 	ServiceAccount:             "tests.serviceAccount",
 	ClusterHealthChecksTimeout: "tests.clusterHealthChecksTimeout",
 	EnableFips:                 "tests.enableFips",
-	EnablePrCheck:              "tests.enablePrCheck",
 }
 
 // Cluster config keys.
@@ -670,9 +665,6 @@ func InitViper() {
 
 	viper.SetDefault(Tests.EnableFips, false)
 	viper.BindEnv(Tests.EnableFips, "ENABLE_FIPS")
-
-	viper.SetDefault(Tests.EnablePrCheck, false)
-	viper.BindEnv(Tests.EnablePrCheck, "ENABLE_PR_CHECK")
 
 	// ----- Cluster -----
 	viper.SetDefault(Cluster.MultiAZ, false)

pkg/e2e/e2e.go
@@ -370,14 +370,6 @@ func runGinkgoTests() (int, error) {
 	// Update the metadata object to use the report directory.
 	metadata.Instance.SetReportDir(reportDir)
 
-	//Checks for existing pr-checker job
-	if viper.GetBool(config.Tests.EnablePrCheck) {
-		if err := cluster.PrCheckQueue(provider); err != nil {
-			getLogs()
-			return Failure, fmt.Errorf("error checking for existing pr-checker job: %v", err)
-		}
-	}
-
 	log.Println("Running e2e tests...")
 
 	if viper.GetString(config.Suffix) == "" {

commit cf3cd5b9465030d7c01ba76ac8dd8478f7f90c07

debug: fix getLastJobID
this was broken in fb6fe3d88f255f983f354ff1415a9632077e2e09 which caused
us to curl an empty string which results in a failure. Put back the
logic to handle the URL based on periodic or PR check.

Signed-off-by: Brady Pratt <bpratt@redhat.com>

pkg/debug/diff.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"sort"
 	"strconv"
 	"strings"
@@ -132,7 +133,11 @@ func GetCurrentMCCHash() (hash string, err error) {
 
 func getLastJobID(baseProwURL, jobName string) (int, error) {
 	// Look up the list of previous jobs with a given name
-	var url string
+	url := fmt.Sprintf("%s/job-history/gs/origin-ci-test/logs/%s", baseProwURL, jobName)
+	if os.Getenv("JOB_TYPE") == "presubmit" {
+		url = fmt.Sprintf("%s/job-history/gs/origin-ci-test/pr-logs/directory/%s", baseProwURL, jobName)
+	}
+
 	log.Printf("Looking up job history from %s", url)
 	res, err := http.Get(url)
 	if err != nil {