81fc8eb4227ce472e6f1cd673fb2a7a4c7047607
Ritu Mundhe <rmundhe@redhat.com>
2024-01-21 15:08:19 -0600

    osde2e cleanup command addition for iam resources

--- cmd/osde2e/cleanup/cmd.go
+++ cmd/osde2e/cleanup/cmd.go
@@ -3,9 +3,11 @@ package cleanup
 import (
 	"fmt"
 	"log"
+	"strconv"
 	"time"
 
 	"github.com/openshift/osde2e/cmd/osde2e/common"
+	"github.com/openshift/osde2e/pkg/common/aws"
 	"github.com/openshift/osde2e/pkg/common/metadata"
 	"github.com/openshift/osde2e/pkg/common/providers"
 	"github.com/openshift/osde2e/pkg/common/spi"
@@ -25,6 +27,9 @@ var args struct {
 	customConfig    string
 	secretLocations string
 	clusterID       string
+	iam             bool
+	olderThan       int
+	dryrun          bool
 }
 
 func init() {
@@ -54,54 +59,90 @@ func init() {
 		"",
 		"A specific cluster id to cleanup",
 	)
-
+	flags.BoolVar(
+		&args.iam,
+		"iam",
+		false,
+		"Cleanup iam resources",
+	)
+	flags.IntVar(
+		&args.olderThan,
+		"older-than",
+		1,
+		"Cleanup iam resources older than this number of days",
+	)
+	flags.BoolVar(
+		&args.dryrun,
+		"dry-run",
+		true,
+		"Show dry run log of deleting iam resources",
+	)
 	Cmd.RegisterFlagCompletionFunc("output-format", func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 		return []string{"json", "prom"}, cobra.ShellCompDirectiveDefault
 	})
 }
 
 func run(cmd *cobra.Command, argv []string) error {
-	var provider spi.Provider
-	var err error
-	if err = common.LoadConfigs(args.configString, args.customConfig, args.secretLocations); err != nil {
-		return fmt.Errorf("error loading initial state: %v", err)
-	}
-
-	if provider, err = providers.ClusterProvider(); err != nil {
-		return fmt.Errorf("could not setup cluster provider: %v", err)
-	}
-
-	metadata.Instance.SetEnvironment(provider.Environment())
-
-	if args.clusterID == "" {
-		clusters, err := provider.ListClusters("properties.MadeByOSDe2e='true'")
-		if err != nil {
-			return err
+	if !args.iam {
+		var provider spi.Provider
+		var err error
+		if err = common.LoadConfigs(args.configString, args.customConfig, args.secretLocations); err != nil {
+			return fmt.Errorf("error loading initial state: %v", err)
 		}
 
-		now := time.Now()
+		if provider, err = providers.ClusterProvider(); err != nil {
+			return fmt.Errorf("could not setup cluster provider: %v", err)
+		}
 
-		for _, cluster := range clusters {
-			if !cluster.ExpirationTimestamp().IsZero() && now.UTC().After(cluster.ExpirationTimestamp().UTC()) {
-				log.Printf("%s %s has expired. Deleting cluster...", cluster.ID(), cluster.Name())
-				if err := provider.DeleteCluster(cluster.ID()); err != nil {
-					log.Printf("Error deleting cluster: %s", err.Error())
+		metadata.Instance.SetEnvironment(provider.Environment())
+
+		if args.clusterID == "" {
+			clusters, err := provider.ListClusters("properties.MadeByOSDe2e='true'")
+			if err != nil {
+				return err
+			}
+
+			now := time.Now()
+
+			for _, cluster := range clusters {
+				if !cluster.ExpirationTimestamp().IsZero() && now.UTC().After(cluster.ExpirationTimestamp().UTC()) {
+					log.Printf("%s %s has expired. Deleting cluster...", cluster.ID(), cluster.Name())
+					if err := provider.DeleteCluster(cluster.ID()); err != nil {
+						log.Printf("Error deleting cluster: %s", err.Error())
+					}
 				}
+			}
+		} else {
+			cluster, err := provider.GetCluster(args.clusterID)
+			if err != nil {
+				log.Printf("Cluster id: %s not found, unable to delete it", args.clusterID)
+				return err
+			}
+
+			log.Printf("Deleting cluster id: %s, name: %s", cluster.ID(), cluster.Name())
+			if err = provider.DeleteCluster(cluster.ID()); err != nil {
+				log.Printf("Failed to delete cluster id: %s, error: %v", cluster.ID(), err)
+				return err
 			}
 		}
 	} else {
-		cluster, err := provider.GetCluster(args.clusterID)
+		hours := strconv.Itoa(args.olderThan * 24)
+		fmthours, err := time.ParseDuration(hours + "h")
 		if err != nil {
-			log.Printf("Cluster id: %s not found, unable to delete it", args.clusterID)
-			return err
+			return fmt.Errorf("Could not parse older-than value to time duration. Please provide a number.")
 		}
-
-		log.Printf("Deleting cluster id: %s, name: %s", cluster.ID(), cluster.Name())
-		if err = provider.DeleteCluster(cluster.ID()); err != nil {
-			log.Printf("Failed to delete cluster id: %s, error: %v", cluster.ID(), err)
-			return err
+		err = aws.CcsAwsSession.CleanupOpenIDConnectProviders(fmthours, args.dryrun)
+		if err != nil {
+			return fmt.Errorf("Could not delete OIDC providers: %s", err.Error())
+		}
+		err = aws.CcsAwsSession.CleanupRoles(fmthours, args.dryrun)
+		if err != nil {
+			return fmt.Errorf("Could not delete IAM roles: %s", err.Error())
+		}
+		err = aws.CcsAwsSession.CleanupPolicies(fmthours, args.dryrun)
+		if err != nil {
+			return fmt.Errorf("Could not delete IAM policies: %s", err.Error())
 		}
 	}
-
 	return nil
 }
