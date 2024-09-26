cd2b899ce5336a8e4d437fe7c6bfdd3b433edc06
Cortney Reed <creed@redhat.com>
2024-09-26 11:57:04 -0400

    [OSD-25354] update validate script

--- test/sdn_migration/scripts/validate-sdn-ovn-migration-jobs.sh
+++ test/sdn_migration/scripts/validate-sdn-ovn-migration-jobs.sh
@@ -6,7 +6,7 @@ UNIQUE_ID=$(date +%s%N) # Generate a unique identifier for this instance
 CONTAINER_NAME="sdn-ovn-migration-${UNIQUE_ID}" # Name of the container based on the unique identifier
 
 # Create the container with environment variables and unique name
-podman create --name "${CONTAINER_NAME}" -e OCM_TOKEN \
+docker create --name "${CONTAINER_NAME}" -e OCM_TOKEN \
 	-e AWS_ACCESS_KEY_ID \
 	-e AWS_SECRET_ACCESS_KEY \
 	-e AWS_REGION \
@@ -23,10 +23,10 @@ podman create --name "${CONTAINER_NAME}" -e OCM_TOKEN \
 
 
 # Start the container
-podman start -a "${CONTAINER_NAME}"
+docker start -a "${CONTAINER_NAME}"
 
 # Copy the junit results xml for publishing
-podman cp "${CONTAINER_NAME}":/tmp/osde2e-report .
+docker cp "${CONTAINER_NAME}":/tmp/osde2e-report .
 
 # Optionally, clean up by removing the container after use
-podman rm "${CONTAINER_NAME}"
+docker rm "${CONTAINER_NAME}"
