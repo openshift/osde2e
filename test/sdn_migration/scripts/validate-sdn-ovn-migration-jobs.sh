df7b5fdd28d0b8f8e2b47fd9e7e1f21a740ccdb9
Cortney Reed <creed@redhat.com>
2024-09-26 09:56:45 -0400

    [OSD-25354] update Dockerfile

--- test/sdn_migration/scripts/validate-sdn-ovn-migration-jobs.sh
+++ test/sdn_migration/scripts/validate-sdn-ovn-migration-jobs.sh
@@ -5,12 +5,6 @@ set +e
 UNIQUE_ID=$(date +%s%N) # Generate a unique identifier for this instance
 CONTAINER_NAME="sdn-ovn-migration-${UNIQUE_ID}" # Name of the container based on the unique identifier
 
-# Check if the container already exists
-if podman ps -a --format '{{.Names}}' | grep -q "^${CONTAINER_NAME}$"; then
-    echo "Container ${CONTAINER_NAME} already exists. Exiting."
-    exit 1
-fi
-
 # Create the container with environment variables and unique name
 podman create --name "${CONTAINER_NAME}" -e OCM_TOKEN \
 	-e AWS_ACCESS_KEY_ID \
