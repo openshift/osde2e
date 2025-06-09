8f5e1d8cc31ce3fefafe7a4500ea45a83db04da7
speddint <speddint@redhat.com>
2025-06-09 16:48:39 -0400

    Added Rosa Billing Account ID

--- scripts/gap-analysis-jobs.sh
+++ scripts/gap-analysis-jobs.sh
@@ -27,6 +27,7 @@ docker create --name "${CONTAINER_NAME}" -e OCM_TOKEN \
 	-e CHANNEL \
 	-e INSTALL_LATEST_XY \
 	-e CLUSTER_VERSION \
+	-e ROSA_BILLING_ACCOUNT_ID \
 	-e GCP_CREDS_JSON \
 	-e INSTALL_LATEST_NIGHTLY \
 	-e REPORT_DIR='/tmp/osde2e-report' \
